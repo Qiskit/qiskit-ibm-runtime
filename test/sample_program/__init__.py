908e819f59143dd671dccb6cbb27db663cee8684
Lucy Xing <88128115+LucyXing@users.noreply.github.com>
2021-08-17 12:52:43 -0400

    63 Add tests (#62)
    * remove vqe program and tests
    
    * add empty test file
    
    * Revert "remove vqe program and tests"
    
    This reverts commit 463986347a660e2bae99f26c880780be6d19e4b6.
    
    * add tests
    
    * address some comments
    
    * address comments
    
    * format
    
    * address comments
    
    * address comments
    
    * address comments and fix lint
    
    * fix serialize inputs
    
    Co-authored-by: Lucy-Xing <Lucy.Xing@ibm.com>

--- test/sample_program/__init__.py
+++ test/sample_program/__init__.py
@@ -10,8 +10,4 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""The VQE runtime program."""
-
-from .vqe_program import VQEProgram
-
-__all__ = ["VQEProgram"]
+"""Runtime test packages."""
---
+++ test/sample_program/test_sample_program.py
@@ -0,0 +1,38 @@
+# This code is part of qiskit-runtime.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test the sample_program."""
+
+import json
+from test.fake_user_messenger import FakeUserMessenger
+from unittest import TestCase
+from qiskit.providers.aer import AerSimulator
+from qiskit.providers.ibmq.runtime.utils import RuntimeEncoder, RuntimeDecoder
+from qiskit_runtime.sample_program import sample_program
+
+
+class TestSampleProgram(TestCase):
+    """Test sample_program."""
+
+    def setUp(self) -> None:
+        """Test case setup."""
+        self.backend = AerSimulator()
+        user_messenger = FakeUserMessenger()
+        self.user_messenger = user_messenger
+
+    def test_sample_program(self):
+        """Test sample program."""
+        inputs = {"iterations": 2}
+        serialized_inputs = json.dumps(inputs, cls=RuntimeEncoder)
+        unserialized_inputs = json.loads(serialized_inputs, cls=RuntimeDecoder)
+        sample_program.main(self.backend, self.user_messenger, **unserialized_inputs)
+        self.assertEqual(self.user_messenger.call_count, inputs["iterations"] + 1)
