8d7bb9a67e9bc42c64cc8f4e34b2a4f8827ff2e3
Daniel Kaulen <dkaulen@de.ibm.com>
2021-11-30 11:39:12 +0100

    Drop support for Python 3.6 and use Python 3.9 by default.
    Why?
    - Python 3.6 will reach end of life soon (Dec' 2021)
    - Python 3.9 is the latest version that qiskit-terra currently supports

--- test/ibm/test_registration.py
+++ test/ibm/test_registration.py
@@ -70,14 +70,6 @@ class TestCredentials(IBMTestCase):
             "No IBM Quantum credentials found", str(context_manager.exception)
         )
 
-    # Test not supported in Python 3.6 since patching a classmethod
-    # (in mock_ibm_provider) incorrectly throws
-    # TypeError: 'NonCallableMagicMock' object is not callable
-    # which was fixed in later versions
-    @skipIf(
-        sys.version_info.major == 3 and sys.version_info.minor == 6,
-        "Test not supported on Python 3.6",
-    )
     def test_store_credentials_overwrite(self) -> None:
         """Test overwriting qiskitrc credentials."""
         credentials = Credentials("QISKITRC_TOKEN", url=QISKIT_IBM_RUNTIME_API_URL)
