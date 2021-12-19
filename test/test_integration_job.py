eabace0344d9f49d5e8362c971d7ad17554e7bf0
jessieyu <jessieyu@us.ibm.com>
2021-12-19 17:44:07 -0500

    fix lint

--- test/test_integration_job.py
+++ test/test_integration_job.py
@@ -657,6 +657,7 @@ class TestIntegrationJob(IBMTestCase):
             self.skipTest(f"Job {job.job_id} unable to reach status {status}.")
 
     def _get_real_device(self, service):
+        # pylint: disable=inconsistent-return-statements
         try:
             return service.least_busy(simulator=False).name()
         except QiskitBackendNotFoundError:
