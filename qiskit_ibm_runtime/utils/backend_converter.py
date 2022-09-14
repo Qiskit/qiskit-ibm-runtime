5f7f2497790ef5c6ca72270ac1d52cb0bd5c2e29
Matthew Treinish <mtreinish@kortar.org>
2022-09-14 16:20:33 -0400

    Add missing import

--- qiskit_ibm_runtime/utils/backend_converter.py
+++ qiskit_ibm_runtime/utils/backend_converter.py
@@ -18,6 +18,7 @@ from qiskit.transpiler.target import Target, InstructionProperties
 from qiskit.utils.units import apply_prefix
 from qiskit.circuit.library.standard_gates import IGate, SXGate, XGate, CXGate, RZGate
 from qiskit.circuit.parameter import Parameter
+from qiskit.circuit.delay import Delay
 from qiskit.circuit.gate import Gate
 from qiskit.circuit.measure import Measure
 from qiskit.circuit.reset import Reset
