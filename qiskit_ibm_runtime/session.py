5528bc93c5fbc93e7bc193ef186f50ebdd56d6b5
kevin-tian <kevin.tian@ibm.com>
2023-01-11 20:32:09 -0500

    fix max_time bug

--- qiskit_ibm_runtime/session.py
+++ qiskit_ibm_runtime/session.py
@@ -143,7 +143,7 @@ class Session:
         options = options or {}
         options["backend"] = self._backend
 
-        if not self._session_id:
+        if not self._session_id and not options.get("max_execution_time"):
             # TODO: What happens if session max time != first job max time?
             # Use session max time if this is first job.
             options["max_execution_time"] = self._max_time
