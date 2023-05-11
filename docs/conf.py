01322247da7fdcc252cef90eda8b30d2152128a9
Eric Arellano <ericarellano@me.com>
2023-05-11 13:22:19 -0600

    Remove unnecessary CSS files

--- docs/conf.py
+++ docs/conf.py
@@ -70,12 +70,10 @@ extensions = [
     'reno.sphinxext',
     'nbsphinx',
     'sphinx_design',
-    "sphinxcontrib.jquery",  # Remove when changing html_theme to qiskit_ecosystem.
+    "qiskit_sphinx_theme",
 ]
 html_static_path = ['_static']
 templates_path = ['_templates']
-
-html_css_files = ['style.css', 'gallery.css']
 
 nbsphinx_timeout = 300
 nbsphinx_execute = "never"
