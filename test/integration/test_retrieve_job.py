2aa1e860c20fbc63ae9e402d3f476d02b92b8309
Kevin Tian <kevin.tian@ibm.com>
2023-06-09 13:40:18 -0400

    Update retrieve job integration tests (#900)
    * update retrieve job integration tests
    
    * refactor test
    
    * typo

--- test/integration/test_retrieve_job.py
+++ test/integration/test_retrieve_job.py
@@ -56,6 +56,7 @@ class TestIntegrationRetrieveJob(IBMIntegrationJobTestCase):
         self.assertEqual(self.program_ids[service.channel], rjob.program_id)
 
     @run_integration_test
+    @quantum_only
     def test_lazy_loading_params(self, service):
         """Test lazy loading job params."""
         job = self._run_program(
@@ -148,13 +149,11 @@ class TestIntegrationRetrieveJob(IBMIntegrationJobTestCase):
     @run_integration_test
     def test_retrieve_jobs_by_job_tags(self, service):
         """Test retrieving jobs by job_tags."""
-        job_tags = ["test_tag"]
+        job_tags = ["job_tag_test"]
         job = self._run_program(service, job_tags=job_tags)
         job.wait_for_final_state()
         rjobs = service.jobs(job_tags=job_tags)
-        self.assertEqual(
-            1, len(rjobs), f"Retrieved jobs: {[j.job_id() for j in rjobs]}"
-        )
+        self.assertIn(job.job_id(), [j.job_id() for j in rjobs])
         rjobs = service.jobs(job_tags=["no_test_tag"])
         self.assertFalse(rjobs)
 
