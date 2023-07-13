ab5a789e8b81c2967eb9fcd4cc5c6425a5e01f8b
kevin-tian <kevin.tian@ibm.com>
2023-07-13 14:15:21 -0400

    Update main branch version 0.11.3

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ author = 'Qiskit Development Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.11.2'
+release = '0.11.3'
 
 docs_url_prefix = "ecosystem/ibm-runtime"
 
