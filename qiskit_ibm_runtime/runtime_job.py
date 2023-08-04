251cc6b797f79f373bd7da5f84555e0a62ef2822
Kevin Tian <kevin.tian@ibm.com>
2023-08-04 14:08:02 -0400

    Update qiskit_ibm_runtime/runtime_job.py
    Co-authored-by: Jessie Yu <jessieyu@us.ibm.com>

--- qiskit_ibm_runtime/runtime_job.py
+++ qiskit_ibm_runtime/runtime_job.py
@@ -662,8 +662,9 @@ class RuntimeJob(Job):
         """Return the usage estimation infromation for this job.
 
         Returns:
-            ``quantum_seconds`` which is the estimated running
-            time of the job in seconds.
+            ``quantum_seconds`` which is the estimated quantum time 
+            of the job in seconds. Quantum time represents the time that 
+            the QPU complex is occupied exclusively by the job.
         """
         if not self._usage_estimation:
             response = self._api_client.job_get(job_id=self.job_id())
