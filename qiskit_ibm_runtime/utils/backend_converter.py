ea1145ae4001f3a68780adced4a4cae494de2909
Kevin Tian <kevin.tian@ibm.com>
2023-09-12 10:37:43 -0400

    Enable datetime parameter for backend properties (#1070)
    * enable datetime param for backend properties
    
    * add test & reno
    
    * improve test

--- qiskit_ibm_runtime/utils/backend_converter.py
+++ qiskit_ibm_runtime/utils/backend_converter.py
@@ -127,7 +127,7 @@ def convert_to_target(
         target.granularity = configuration.timing_constraints.get("granularity")
         target.min_length = configuration.timing_constraints.get("min_length")
         target.pulse_alignment = configuration.timing_constraints.get("pulse_alignment")
-        target.aquire_alignment = configuration.timing_constraints.get("acquire_alignment")
+        target.acquire_alignment = configuration.timing_constraints.get("acquire_alignment")
     # If pulse defaults exists use that as the source of truth
     if defaults is not None:
         faulty_qubits = set()
