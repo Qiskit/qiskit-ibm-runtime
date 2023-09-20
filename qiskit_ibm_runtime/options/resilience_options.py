af0c51fe6f01fac3346b4382d342324ba9225905
Jessie Yu <jessieyu@us.ibm.com>
2023-09-20 15:55:52 -0400

    black

--- qiskit_ibm_runtime/options/resilience_options.py
+++ qiskit_ibm_runtime/options/resilience_options.py
@@ -117,7 +117,10 @@ class ResilienceOptions:
     # ZNE
     zne_mitigation: bool = None
     zne_noise_factors: Sequence[float] = None
-    zne_extrapolator: Union[ZneExtrapolatorType, Sequence[ZneExtrapolatorType]] = ("exponential", "linear")
+    zne_extrapolator: Union[ZneExtrapolatorType, Sequence[ZneExtrapolatorType]] = (
+        "exponential",
+        "linear",
+    )
     zne_stderr_threshold: float = None
 
     # PEC
