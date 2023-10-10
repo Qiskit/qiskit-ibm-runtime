2f31c3abb88295b0cc4898dc5a65f3901e87dea1
kevin-tian <kevin.tian@ibm.com>
2023-10-10 17:03:19 -0400

    fix indent

--- qiskit_ibm_runtime/session.py
+++ qiskit_ibm_runtime/session.py
@@ -227,7 +227,7 @@ class Session:
             id: id of the session.
             backend_name: backend used for the session.
             interactive_timeout: The maximum idle time (in seconds) between jobs that
-                is allowed to occur before the session is deactivated.
+            is allowed to occur before the session is deactivated.
             max_time: Maximum allowed time (in seconds) for the session, subject to plan limits.
             active_timeout: The maximum time (in seconds) a session can stay active.
             state: State of the session - open, active, inactive, or closed.
