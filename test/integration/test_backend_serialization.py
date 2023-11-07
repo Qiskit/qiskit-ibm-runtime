fc7e44d2a01c2d7a8c0c08a8cb186a062073cee8
kevin-tian <kevin.tian@ibm.com>
2023-11-07 10:58:23 -0500

    Update backend serialization test

--- test/integration/test_backend_serialization.py
+++ test/integration/test_backend_serialization.py
@@ -17,14 +17,13 @@ from typing import Any, Dict, Set, Optional
 import dateutil.parser
 
 from ..ibm_test_case import IBMIntegrationTestCase
-from ..decorators import (
-    run_integration_test,
-)
+from ..decorators import run_integration_test, production_only
 
 
 class TestSerialization(IBMIntegrationTestCase):
     """Test data serialization."""
 
+    @production_only
     @run_integration_test
     def test_backend_configuration(self, service):
         """Test deserializing backend configuration."""
