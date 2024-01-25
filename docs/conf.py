b0cf6e685462d52c98eb4551db10acba66324829
kevin-tian <kevin.tian@ibm.com>
2024-01-25 12:35:35 -0500

    Update main branch 0.18.1

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ language = 'en'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.18.0'
+release = '0.18.1'
 
 # -- General configuration ---------------------------------------------------
 
