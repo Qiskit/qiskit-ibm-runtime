9cb2532f84ff98383e112874bd268c479fa540f4
kevin-tian <kevin.tian@ibm.com>
2024-03-01 18:07:24 -0500

    Add dosctring for Batch class

--- qiskit_ibm_runtime/batch.py
+++ qiskit_ibm_runtime/batch.py
@@ -20,7 +20,43 @@ from .session import Session
 
 
 class Batch(Session):
-    """Class for creating a batch mode in Qiskit Runtime."""
+    """Class for creating a batch mode in Qiskit Runtime.
+
+    Just like `session`, a Qiskit Runtime ``batch`` allows you to group a collection of 
+    iterative calls to the quantum computer. Batch mode can shorten processing time if all jobs 
+    can be provided at the outset. If you want to submit iterative jobs, use sessions instead. 
+
+    Using batch mode has these benefits:
+        - The jobs' classical computation, such as compilation, is run in parallel. 
+          Thus, running multiple jobs in a batch is significantly faster than running them serially.
+
+        - There is no delay between job, which can help avoid drift.
+
+    You can open a Qiskit Runtime batch using this ``Batch`` class and submit jobs
+    to one or more primitives.
+
+    For example::
+
+        from qiskit.circuit import QuantumCircuit, QuantumRegister, ClassicalRegister
+        from qiskit_ibm_runtime import Sampler, Batch, Options
+
+        # Bell Circuit
+        qr = QuantumRegister(2, name="qr")
+        cr = ClassicalRegister(2, name="cr")
+        qc = QuantumCircuit(qr, cr, name="bell")
+        qc.h(qr[0])
+        qc.cx(qr[0], qr[1])
+        qc.measure(qr, cr)
+
+        options = Options(optimization_level=3)
+
+        with Batch(backend="ibmq_qasm_simulator") as batch:
+            sampler = Sampler(batch, options=options)
+            job = sampler.run(qc)
+            print(f"Sampler job ID: {job.job_id()}")
+            print(f"Sampler job result: {job.result()}")
+
+    """
 
     def __init__(
         self,
@@ -28,6 +64,26 @@ class Batch(Session):
         backend: Optional[Union[str, IBMBackend]] = None,
         max_time: Optional[Union[int, str]] = None,
     ):
+        """Batch constructor.
+
+        Args:
+            service: Optional instance of the ``QiskitRuntimeService`` class.
+                If ``None``, the service associated with the backend, if known, is used.
+                Otherwise ``QiskitRuntimeService()`` is used to initialize
+                your default saved account.
+            backend: Optional instance of :class:`qiskit_ibm_runtime.IBMBackend` class or
+                string name of backend. An instance of :class:`qiskit_ibm_provider.IBMBackend` will not work.
+
+            max_time: (EXPERIMENTAL setting, can break between releases without warning)
+                Maximum amount of time, a runtime session can be open before being
+                forcibly closed. Can be specified as seconds (int) or a string like "2h 30m 40s".
+                This value must be less than the
+                `system imposed maximum
+                <https://docs.quantum.ibm.com/run/max-execution-time>`_.
+
+        Raises:
+            ValueError: If an input value is invalid.
+        """
         super().__init__(service=service, backend=backend, max_time=max_time)
 
     def _create_session(self) -> str:
