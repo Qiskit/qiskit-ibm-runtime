2bc70eab76f3b523fc78cecab47b555533b96141
kevin-tian <kevin.tian@ibm.com>
2024-03-11 11:17:06 -0400

    refactor

--- qiskit_ibm_runtime/runtime_job_v2.py
+++ qiskit_ibm_runtime/runtime_job_v2.py
@@ -187,7 +187,7 @@ class RuntimeJobV2(BasePrimitiveJob[PrimitiveResult, JobStatus], BaseRuntimeJob)
         """
         api_status = response["state"]["status"].upper()
         if api_status in API_TO_JOB_STATUS:
-            mapped_job_status = API_TO_JOB_STATUS[response["state"]["status"].upper()]
+            mapped_job_status = API_TO_JOB_STATUS[api_status]
             if mapped_job_status == "CANCELLED" and self._reason == "RAN TOO LONG":
                 mapped_job_status = "ERROR"
             return mapped_job_status
