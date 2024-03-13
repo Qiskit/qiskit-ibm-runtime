9598d789f778233a5a8a53718b09f398affa00cb
Arnau Casau <arnaucasau@gmail.com>
2024-03-13 14:52:19 +0100

    use Path

--- tools/concat_release_notes.py
+++ tools/concat_release_notes.py
@@ -1,9 +1,9 @@
 from packaging import version
-from pathlib import Path, PurePosixPath
+from pathlib import Path
 
 
 def get_root_path() -> Path:
-    return PurePosixPath(Path(__file__).absolute()).parent.parent
+    return Path(Path(__file__).absolute()).parent.parent
 
 
 def get_version_from_path(path: Path) -> str:
