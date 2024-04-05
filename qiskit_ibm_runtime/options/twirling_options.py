a8367fe4a19f10c71eba214b44bc7050e83645a3
ptristan <paula.mariela.tristan@ibm.com>
2024-04-05 14:07:29 -0300

    updated docstring from PR comment

--- qiskit_ibm_runtime/options/twirling_options.py
+++ qiskit_ibm_runtime/options/twirling_options.py
@@ -32,8 +32,8 @@ class TwirlingOptions:
     Args:
         enable_gates: Whether to apply 2-qubit gate twirling. Default: False.
 
-        enable_measure: Whether to enable twirling of expectation value measurements
-          in the Primitive. Default: True.
+        enable_measure: Whether to enable twirling of measurements. The twirling can only be applied to
+         those measurement registers not involved within a conditional logic. Default: True.
 
         num_randomizations: The number of random samples to use when twirling or
             peforming sampled mitigation. If "auto", the value will be chosen automatically
