88b1fedff3888aed1347611851cb193cd1811ada
ptristan3 <44805021+ptristan3@users.noreply.github.com>
2024-04-08 17:03:41 -0300

    Update qiskit_ibm_runtime/sampler.py
    Co-authored-by: Jessie Yu <jessieyu@us.ibm.com>

--- qiskit_ibm_runtime/sampler.py
+++ qiskit_ibm_runtime/sampler.py
@@ -109,7 +109,7 @@ class SamplerV2(BasePrimitiveV2[SamplerOptions], Sampler, BaseSamplerV2):
             deprecate_arguments(
                 "session",
                 "0.23.0",
-                "The session param is going to be consolidated to the mode param.",
+                "Please use the 'mode' parameter instead.",
             )
         if isinstance(mode, str) or isinstance(backend, str):
             issue_deprecation_msg(
