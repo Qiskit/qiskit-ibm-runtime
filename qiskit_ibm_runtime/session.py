9c77f948ab443635875ced6153a5a9e45617bf1b
kevin-tian <kevin.tian@ibm.com>
2024-04-15 14:54:21 -0400

    Fix Session docstring formatting

--- qiskit_ibm_runtime/session.py
+++ qiskit_ibm_runtime/session.py
@@ -248,12 +248,13 @@ class Session:
 
         Returns:
             The current status of the session, including:
-            Pending: Session is created but not active.
-            It will become active when the next job of this session is dequeued.
-            In progress, accepting new jobs: session is active and accepting new jobs.
-            In progress, not accepting new jobs: session is active and not accepting new jobs.
-            Closed: max_time expired or session was explicitly closed.
-            None: status details are not available.
+
+            * Pending: Session is created but not active.
+              It will become active when the next job of this session is dequeued.
+            * In progress, accepting new jobs: session is active and accepting new jobs.
+            * In progress, not accepting new jobs: session is active and not accepting new jobs.
+            * Closed: max_time expired or session was explicitly closed.
+            * None: status details are not available.
         """
         details = self.details()
         if details:
@@ -274,22 +275,23 @@ class Session:
 
         Returns:
             A dictionary with the sessions details, including:
-            id: id of the session.
-            backend_name: backend used for the session.
-            interactive_timeout: The maximum idle time (in seconds) between jobs that
-            is allowed to occur before the session is deactivated.
-            max_time: Maximum allowed time (in seconds) for the session, subject to plan limits.
-            active_timeout: The maximum time (in seconds) a session can stay active.
-            state: State of the session - open, active, inactive, or closed.
-            accepting_jobs: Whether or not the session is accepting jobs.
-            last_job_started: Timestamp of when the last job in the session started.
-            last_job_completed: Timestamp of when the last job in the session completed.
-            started_at: Timestamp of when the session was started.
-            closed_at: Timestamp of when the session was closed.
-            activated_at: Timestamp of when the session state was changed to active.
-            mode: Execution mode of the session.
-            usage_time: The usage time, in seconds, of this Session or Batch.
-            Usage is defined as the time a quantum system is committed to complete a job.
+
+            * id: id of the session.
+            * backend_name: backend used for the session.
+            * interactive_timeout: The maximum idle time (in seconds) between jobs that
+              is allowed to occur before the session is deactivated.
+            * max_time: Maximum allowed time (in seconds) for the session, subject to plan limits.
+            * active_timeout: The maximum time (in seconds) a session can stay active.
+            * state: State of the session - open, active, inactive, or closed.
+            * accepting_jobs: Whether or not the session is accepting jobs.
+            * last_job_started: Timestamp of when the last job in the session started.
+            * last_job_completed: Timestamp of when the last job in the session completed.
+            * started_at: Timestamp of when the session was started.
+            * closed_at: Timestamp of when the session was closed.
+            * activated_at: Timestamp of when the session state was changed to active.
+            * mode: Execution mode of the session.
+            * usage_time: The usage time, in seconds, of this Session or Batch.
+              Usage is defined as the time a quantum system is committed to complete a job.
         """
         if self._session_id and isinstance(self._service, QiskitRuntimeService):
             response = self._service._api_client.session_details(self._session_id)
