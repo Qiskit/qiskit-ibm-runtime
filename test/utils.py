e8b9f6b1d8bf668b8a5f175d898015a251004965
Takashi Imamichi <imamichi@jp.ibm.com>
2024-04-15 16:15:35 +0900

    copies unit tests on faulty qubits from Qiskit

--- test/utils.py
+++ test/utils.py
@@ -238,6 +238,7 @@ def create_faulty_backend(
     model_backend: Backend,
     faulty_qubit: Optional[int] = None,
     faulty_edge: Optional[tuple] = None,
+    faulty_q1_property: Optional[int] = None,
 ) -> IBMBackend:
     """Create an IBMBackend that has faulty qubits and/or edges.
 
@@ -245,6 +246,7 @@ def create_faulty_backend(
         model_backend: Fake backend to model after.
         faulty_qubit: Faulty qubit.
         faulty_edge: Faulty edge, a tuple of (gate, qubits)
+        faulty_q1_property: Faulty Q1 property.
 
     Returns:
         An IBMBackend with faulty qubits/edges.
@@ -269,6 +271,11 @@ def create_faulty_backend(
                         "value": 0,
                     }
                 )
+
+    if faulty_q1_property:
+        properties["qubits"][faulty_q1_property] = [
+            q for q in properties["qubits"][faulty_q1_property] if q["name"] != "T1"
+        ]
 
     out_backend = IBMBackend(
         configuration=model_backend.configuration(),
