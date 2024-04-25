72da3736602cc4197ccc08036f35ae79bc0c0a05
kevin-tian <kevin.tian@ibm.com>
2024-04-25 16:45:32 -0400

    Raise better error if service is down

--- qiskit_ibm_runtime/qiskit_runtime_service.py
+++ qiskit_ibm_runtime/qiskit_runtime_service.py
@@ -42,11 +42,7 @@ from .api.clients.runtime import RuntimeClient
 from .api.exceptions import RequestsApiError
 from .constants import QISKIT_IBM_RUNTIME_API_URL
 from .exceptions import IBMNotAuthorizedError, IBMInputValueError, IBMAccountError
-from .exceptions import (
-    IBMRuntimeError,
-    RuntimeProgramNotFound,
-    RuntimeJobNotFound,
-)
+from .exceptions import IBMRuntimeError, RuntimeProgramNotFound, RuntimeJobNotFound, IBMApiError
 from .hub_group_project import HubGroupProject  # pylint: disable=cyclic-import
 from .utils.result_decoder import ResultDecoder
 from .runtime_job import RuntimeJob
@@ -165,7 +161,11 @@ class QiskitRuntimeService(Provider):
                 )
             self._client_params.token = auth_client.current_access_token()
             self._api_client = RuntimeClient(self._client_params)
-            self._hgps = self._initialize_hgps(auth_client)
+            try:
+                self._hgps = self._initialize_hgps(auth_client)
+            except (json.decoder.JSONDecodeError):
+                raise (IBMApiError("Unexpected response received from server"))
+
             for hgp in self._hgps.values():
                 for backend_name in hgp.backends:
                     if backend_name not in self._backends:
