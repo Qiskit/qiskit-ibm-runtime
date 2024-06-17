e66540d403ef789a2ca4485f58f50d43fb633342
Samuele Ferracin <samferracin@Samueles-MacBook-Pro.local>
2024-06-17 16:25:17 -0400

    added ceil

--- qiskit_ibm_runtime/options/twirling_options.py
+++ qiskit_ibm_runtime/options/twirling_options.py
@@ -38,16 +38,16 @@ class TwirlingOptions:
 
         num_randomizations: The number of random samples to use when twirling or
             peforming sampled mitigation. If `num_randomizations` is "auto":
-                - If `shots_per_randomization` is not "auto", the value is set to `shots/shots_per_randomization`,
-                  where `shots` is the total number of shots.
+                - If `shots_per_randomization` is not "auto", the value is set to `ceil(shots/shots_per_randomization)`,
+                  where `shots` is the total number of shots per circuit and `ceil` is the ceiling function.
                 - If `shots_per_randomization` is "auto", first `shots_per_randomization` is set as described
-                  below, then `num_randomizations` is set as `shots/shots_per_randomization`.
+                  below, then `num_randomizations` is set as `ceil(shots/shots_per_randomization)`.
             Default: "auto".
 
         shots_per_randomization: The number of shots to run for each random sample. If "auto":
-                - If `num_randomizations` is not "auto", the value is set to `shots/num_randomizations`,
-                  where `shots` is the total number of shots.
-                - If `num_randomizations` is "auto", it is set to `64` for PEC circuits and to `max(64, shots / 32)`
+                - If `num_randomizations` is not "auto", the value is set to `ceil(shots/num_randomizations)`,
+                  where `shots` is the total number of shots per circuit and `ceil` is the ceiling function.
+                - If `num_randomizations` is "auto", it is set to `64` for PEC circuits and to `max(64, ceil(shots / 32))`
                   for all other circuits
             Default: "auto".
 
