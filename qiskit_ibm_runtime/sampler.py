86a1560829820c34733992a2ea12153dee1df5ba
Kevin Tian <kevin.tian@ibm.com>
2024-07-17 11:53:02 -0400

    Deprecate passing backend as a string (#1804)
    * Deprecate passing backend as a string
    
    * add release note

--- qiskit_ibm_runtime/sampler.py
+++ qiskit_ibm_runtime/sampler.py
@@ -122,6 +122,14 @@ class SamplerV2(BasePrimitiveV2[SamplerOptions], Sampler, BaseSamplerV2):
                 " the service using `QiskitRuntimeService().backend('ibm_backend')`",
                 3,
             )
+            issue_deprecation_msg(
+                msg="Passing a backend as a string is deprecated",
+                version="0.26.0",
+                remedy="Use the actual backend object instead.",
+                period="3 months",
+                stacklevel=2,
+            )
+
         if mode is None:
             mode = session if backend and session else backend if backend else session
         BasePrimitiveV2.__init__(self, mode=mode, options=options)
