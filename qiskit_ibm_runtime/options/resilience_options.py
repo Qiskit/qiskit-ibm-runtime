f62c2ddb9d82425c4c5f7ae935340b4211e44afc
kevin-tian <kevin.tian@ibm.com>
2024-07-18 16:14:23 -0400

    fix lint

--- qiskit_ibm_runtime/options/resilience_options.py
+++ qiskit_ibm_runtime/options/resilience_options.py
@@ -39,8 +39,9 @@ class ResilienceOptionsV2:
 
     Args:
         measure_mitigation: Whether to enable measurement error mitigation method.
-            If you enable measurement mitigation, you can fine tune its noise learning by using :attr:`~measure_noise_learning`.
-            See :class:`MeasureNoiseLearningOptions` for all measurement mitigation noise learning options.
+            If you enable measurement mitigation, you can fine tune its noise learning
+            by using :attr:`~measure_noise_learning`. See :class:`MeasureNoiseLearningOptions`
+            for all measurement mitigation noise learning options.
             Default: True.
 
         measure_noise_learning: Additional measurement noise learning options.
