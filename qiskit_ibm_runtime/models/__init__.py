f6464f770539f70c04265484a8f66b70668455ae
ptristan <paula.mariela.tristan@ibm.com>
2024-07-29 09:42:06 -0300

    remove unused class

--- qiskit_ibm_runtime/models/__init__.py
+++ qiskit_ibm_runtime/models/__init__.py
@@ -21,5 +21,4 @@ from .backend_configuration import (
 )
 from .backend_properties import BackendProperties, GateProperties, Nduv
 from .backend_status import BackendStatus
-from .job_status import JobStatus
 from .pulse_defaults import PulseDefaults
