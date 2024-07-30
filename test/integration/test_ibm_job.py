5bfdb2d1d6ca60053880406229bd909d95eb3cee
Kevin Tian <kevin.tian@ibm.com>
2024-07-30 10:42:57 -0400

    Update `test_retrieve_pending_jobs` (#1825)

--- test/integration/test_ibm_job.py
+++ test/integration/test_ibm_job.py
@@ -121,12 +121,13 @@ class TestIBMJob(IBMIntegrationTestCase):
 
     def test_retrieve_pending_jobs(self):
         """Test retrieving jobs with the pending filter."""
-        if self.dependencies.channel == "ibm_quantum":
-            raise SkipTest(
-                "Intermittently failing on ibm quantum channel, needs more investigation."
-            )
+        yesterday = datetime.now() - timedelta(days=1)
         pending_job_list = self.service.jobs(
-            program_id="sampler", limit=3, pending=True, created_after=self.last_month
+            program_id="sampler",
+            limit=3,
+            pending=True,
+            created_after=self.last_month,
+            created_before=yesterday,
         )
         for job in pending_job_list:
             self.assertTrue(
