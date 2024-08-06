9729d3a9b0243cee7dde869181b0389e2f915043
SamFerracin <sam.ferracin@ibm.com>
2024-08-06 09:14:37 -0400

    added classes and json

--- qiskit_ibm_runtime/utils/noise_learner_result.py
+++ qiskit_ibm_runtime/utils/noise_learner_result.py
@@ -14,47 +14,110 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
-from typing import Any, Iterator, Sequence
+from typing import Any, Iterator, Optional, Sequence, Union
 
 from qiskit.circuit import QuantumCircuit
 from qiskit.quantum_info import PauliList
 
 
-@dataclass(frozen=True)
-class NoiseLearnerDatum:
-    """A container for a single noise learner datum.
+class PauliLindbladError:
+    """
+    A container for the generators and rates of an error channel in the sparse Pauli-Lindblad noise
+    model.
+
+    Args:
+        paulis: A list of the Pauli generators for the error channel.
+        rates: A list of the rates for the Pauli-Lindblad ``paulis``.
+
+    Raises:
+        ValueError: If ``paulis`` and ``rates`` have different lengths.
+    """
+
+    def __init__(self, paulis: PauliList, rates: Sequence[float]) -> None:
+        if len(paulis) != len(rates):
+            msg = f"``paulis`` has length {len(paulis)} but ``rates`` has length {len(rates)}."
+            raise ValueError(msg)
+
+        self._paulis = paulis
+        self._rates = rates
+
+    @property
+    def paulis(self) -> PauliList:
+        r"""
+        The Pauli generators of this :class:`~.PauliLindbladError`.
+        """
+        return self._paulis
+
+    @property
+    def rates(self) -> Sequence[float]:
+        r"""
+        The rates of this :class:`~.PauliLindbladError`.
+        """
+        return self._rates
+
+    @property
+    def num_qubits(self):
+        r"""
+        The number of qubits in this :class:`~.PauliLindbladError`.
+        """
+        return self.paulis.num_qubits
+
+    def __repr__(self) -> str:
+        return f"PauliLindbladError(paulis={self.paulis}, rates={self.rates})"
+
+
+class LayerError:
+    """The error channel (in Pauli-Lindblad format) of a single layer of instructions.
 
     Args:
         circuit: A circuit whose noise has been learnt.
-        qubits: The labels of the qubits in the given circuit.
-        generators: The list of generators for the Pauli errors.
-        rates: The rates for the given Pauli error generators.
+        qubits: The labels of the qubits in the ``circuit``.
+        error: The Pauli Lindblad error channel affecting the ``circuit``.
 
     Raises:
-        ValueError: If ``circuit``, ``qubits``, and ``generators`` have mismatching number of
-            qubits.
-        ValueError: If ``generators`` and ``rates`` have different lengths.
+        ValueError: If ``circuit``, ``qubits``, and ``error`` have mismatching number of qubits.
     """
 
-    circuit: QuantumCircuit
-    qubits: Sequence[int]
-    generators: PauliList
-    rates: Sequence[float]
-
-    def __post_init__(self) -> None:
-        if len({self.circuit.num_qubits, len(self.qubits), self.generators.num_qubits}) != 1:
+    def __init__(
+        self, circuit: QuantumCircuit, qubits: Sequence[int], error: PauliLindbladError
+    ) -> None:
+        if len({circuit.num_qubits, len(qubits), error.num_qubits}) != 1:
             raise ValueError("Mistmatching numbers of qubits.")
-        if len(self.generators) != len(self.rates):
-            msg = f"``generators`` has length {len(self.generators)}, but "
-            msg += f"``rates`` has length {len(self.rates)}."
-            raise ValueError(msg)
+
+        self._circuit = circuit
+        self._qubits = qubits
+        self._error = error
+
+    @property
+    def circuit(self) -> QuantumCircuit:
+        r"""
+        The circuit in this :class:`LayerError`.
+        """
+        return self._circuit
+
+    @property
+    def qubits(self) -> Sequence[int]:
+        r"""
+        The qubits in this :class:`LayerError`.
+        """
+        return self._qubits
+
+    @property
+    def error(self) -> PauliLindbladError:
+        r"""
+        The error channel in this :class:`LayerError`.
+        """
+        return self._error
+
+    def __repr__(self) -> str:
+        ret = f"circuit={repr(self.circuit)}, qubits={self.qubits}, error={self.error})"
+        return f"LayerError({ret})"
 
 
 class NoiseLearnerResult:
     """A container for the results of a noise learner experiment."""
 
-    def __init__(self, data: Sequence[NoiseLearnerDatum], metadata: dict[str, Any] | None = None):
+    def __init__(self, data: Sequence[LayerError], metadata: dict[str, Any] | None = None):
         """
         Args:
             data: The data of a noise learner experiment.
@@ -65,7 +128,7 @@ class NoiseLearnerResult:
         self._metadata = metadata.copy() or {}
 
     @property
-    def data(self) -> Sequence[NoiseLearnerDatum]:
+    def data(self) -> Sequence[LayerError]:
         """The data of this noise learner result."""
         return self._data
 
@@ -74,7 +137,7 @@ class NoiseLearnerResult:
         """The metadata of this noise learner result."""
         return self._metadata
 
-    def __getitem__(self, index: int) -> NoiseLearnerDatum:
+    def __getitem__(self, index: int) -> LayerError:
         return self.data[index]
 
     def __len__(self) -> int:
@@ -83,5 +146,5 @@ class NoiseLearnerResult:
     def __repr__(self) -> str:
         return f"NoiseLearnerResult(data={self.data}, metadata={self.metadata})"
 
-    def __iter__(self) -> Iterator[NoiseLearnerDatum]:
+    def __iter__(self) -> Iterator[LayerError]:
         return iter(self.data)
