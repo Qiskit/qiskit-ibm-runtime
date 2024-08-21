0bdd307792c23fa12605fee29cf725e3f15c1e94
SamFerracin <sam.ferracin@ibm.com>
2024-08-21 10:55:35 -0400

    disable=too-many-return-statements

--- qiskit_ibm_runtime/visualization/utils.py
+++ qiskit_ibm_runtime/visualization/utils.py
@@ -9,6 +9,7 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+# pylint: disable=too-many-return-statements
 
 """
 Utility functions for visualizing qiskit-ibm-runtime's objects.
