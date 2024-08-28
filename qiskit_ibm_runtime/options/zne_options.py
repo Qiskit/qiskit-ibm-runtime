b6b1fd6f49ae25e9da26e8e966dc037a379f28f7
Samuele Ferracin <sam.ferracin@ibm.com>
2024-08-28 17:22:16 -0400

    Update qiskit_ibm_runtime/options/zne_options.py
    Co-authored-by: Ian Hincks <ian.hincks@gmail.com>

--- qiskit_ibm_runtime/options/zne_options.py
+++ qiskit_ibm_runtime/options/zne_options.py
@@ -113,7 +113,7 @@ class ZneOptions:
                   as :math:`f(x; A, \tau) = A e^{-x/\tau}`, where :math:`A = f(0; A, \tau)` is the
                   value at zero noise (:math:`x=0`) and :math:`\tau>0` is a positive rate.
                 * ``"double_exponential"``, which uses a sum of two exponential as in Ref. 1.
-                * "polynomial_degree_(1 <= k <= 7)", which uses a polynomial function defined as
+                * ``"polynomial_degree_(1 <= k <= 7)"``, which uses a polynomial function defined as
                   :math:`f(x; c_0, c_1, \\ldots, c_k) = \\sum_{i=0, k} c_i x^i`.
                 * ``"linear"``, which is equivalent to "polynomial_degree_1".
 
