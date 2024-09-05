2ed2124f51d01a0e520969eea0e21b1be636fd08
SamFerracin <sam.ferracin@ibm.com>
2024-09-05 10:00:14 -0400

    renamed

--- qiskit_ibm_runtime/transpiler/passes/cliffordization/convert_isa_to_clifford.py
+++ qiskit_ibm_runtime/transpiler/passes/cliffordization/convert_isa_to_clifford.py
@@ -11,7 +11,7 @@
 # that they have been altered from the originals.
 
 """
-Pass to convert the gates of a circuit to Clifford gates.
+Pass to convert the gates of an ISA circuit to Clifford gates.
 """
 
 from random import choices
