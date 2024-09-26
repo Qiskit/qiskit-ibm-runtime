8b38d1ee5b69491243f9cb07ab36e5dedf77fed3
Yael Ben-Haim <yaelbh@il.ibm.com>
2024-09-26 17:08:04 +0300

    Enhance the ISA check with validation of rzz angle (#1953)
    * Enhance the ISA check with validation of rzz angle
    
    * lint
    
    * lint

--- qiskit_ibm_runtime/utils/utils.py
+++ qiskit_ibm_runtime/utils/utils.py
@@ -22,6 +22,7 @@ from queue import Queue
 from threading import Condition
 from typing import List, Optional, Any, Dict, Union, Tuple
 from urllib.parse import urlparse
+import numpy as np
 
 import requests
 from ibm_cloud_sdk_core.authenticators import (  # pylint: disable=import-error
@@ -66,6 +67,16 @@ def _is_isa_circuit_helper(circuit: QuantumCircuit, target: Target, qubit_map: D
             return (
                 f"The instruction {name} on qubits {qargs} is not supported by the target system."
             )
+
+        # rzz gate is calibrated only for the range [0, pi/2].
+        # We allow an angle value of a bit more than pi/2, to compensate floating point rounding
+        # errors (beyond pi/2 does not trigger an error down the stack, only may become less
+        # accurate).
+        if name == "rzz" and (
+            instruction.params[0] < 0.0 or instruction.params[0] > 1.001 * np.pi / 2
+        ):
+            return f"The instruction {name} on qubits {qargs} is supported only for angles in the \
+            range [0, pi/2], but an angle of {instruction.params[0]} has been provided."
 
         if isinstance(operation, ControlFlowOp):
             for sub_circ in operation.blocks:
