02259c0626b678244a4f50d587d302155b3a8ccf
kevin-tian <kevin.tian@ibm.com>
2024-09-30 10:29:16 -0400

    Add backend refresh() method

--- qiskit_ibm_runtime/ibm_backend.py
+++ qiskit_ibm_runtime/ibm_backend.py
@@ -357,6 +357,12 @@ class IBMBackend(Backend):
         self._convert_to_target(refresh=True)
         return self._target
 
+    def refresh(self) -> None:
+        """Refresh the current backend target"""
+        self._get_properties(datetime=python_datetime.now())
+        self._get_defaults()
+        self._convert_to_target(refresh=True)
+
     def properties(
         self, refresh: bool = False, datetime: Optional[python_datetime] = None
     ) -> Optional[BackendProperties]:
