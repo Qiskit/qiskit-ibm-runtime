2bfd24d11f5c3b43b55155fb98cd0a745a93df28
Yael Ben-Haim <yaelbh@il.ibm.com>
2024-11-12 11:53:22 +0200

    check fixed angles also in is_isa_circuit

--- qiskit_ibm_runtime/utils/utils.py
+++ qiskit_ibm_runtime/utils/utils.py
@@ -71,6 +71,20 @@ def _is_isa_circuit_helper(circuit: QuantumCircuit, target: Target, qubit_map: D
             return (
                 f"The instruction {name} on qubits {qargs} is not supported by the target system."
             )
+        
+        # rzz gate is calibrated only for the range [0, pi/2].
+        # We allow an angle value of a bit more than pi/2, to compensate floating point rounding
+        # errors (beyond pi/2 does not trigger an error down the stack, only may become less
+        # accurate).
+        if (
+            name == "rzz"
+            and not isinstance((param := instruction.operation.params[0]), ParameterExpression)
+            and (param < 0.0 or param > 1.001 * np.pi / 2)
+        ):
+            return (
+                f"The instruction {name} on qubits {qargs} is supported only for angles in the "
+                f"range [0, pi/2], but an angle of {param} has been provided."
+            )
 
         if isinstance(operation, ControlFlowOp):
             for sub_circ in operation.blocks:
