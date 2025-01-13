f31360fd83e9d9ebc4219efb497676c3175fe9a6
kevin-tian <kevin.tian@ibm.com>
2025-01-13 12:50:38 -0500

    wip use cloud access token

--- qiskit_ibm_runtime/api/auth.py
+++ qiskit_ibm_runtime/api/auth.py
@@ -14,9 +14,11 @@
 
 from typing import Dict
 
-
-from requests import PreparedRequest
+from requests import PreparedRequest, post
 from requests.auth import AuthBase
+from ..exceptions import IBMNotAuthorizedError
+
+CLOUD_IAM_URL = "https://iam.cloud.ibm.com/identity/token"
 
 
 class CloudAuth(AuthBase):
@@ -25,6 +27,7 @@ class CloudAuth(AuthBase):
     def __init__(self, api_key: str, crn: str):
         self.api_key = api_key
         self.crn = crn
+        self.access_token = self._get_access_token()
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, CloudAuth):
@@ -40,9 +43,21 @@ class CloudAuth(AuthBase):
         r.headers.update(self.get_headers())
         return r
 
+    def _get_access_token(self) -> str:
+        """Return IBM Cloud access token."""
+        try:
+            headers = {"Content-Type": "application/x-www-form-urlencoded"}
+            data = f"grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey={self.api_key}"
+            response = post(CLOUD_IAM_URL, headers=headers, data=data, timeout=10).json()
+            return response["access_token"]
+        except Exception as ex:  # pylint: disable=broad-except
+            raise IBMNotAuthorizedError(
+                "Unable to retrieve IBM Cloud access token. Please check your credentials"
+            ) from ex
+
     def get_headers(self) -> Dict:
         """Return authorization information to be stored in header."""
-        return {"Service-CRN": self.crn, "Authorization": f"apikey {self.api_key}"}
+        return {"Service-CRN": self.crn, "Authorization": f"Bearer {self.access_token}"}
 
 
 class QuantumAuth(AuthBase):
