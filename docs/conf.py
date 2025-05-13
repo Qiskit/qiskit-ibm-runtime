c407f8abce1f198124835c8038902e86193979e9
kevin-tian <kevin.tian@ibm.com>
2025-05-13 11:59:33 -0400

    Prepare release 0.39.0

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ language = 'en'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.38.0'
+release = '0.39.0'
 
 # -- General configuration ---------------------------------------------------
 
