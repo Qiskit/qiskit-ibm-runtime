56b15fbbe518d3285476fc898e452abcea6615a6
Yael Ben-Haim <yaelbh@il.ibm.com>
2025-06-23 13:48:05 +0300

    Starting a feature branch box_preview

---
+++ qiskit_ibm_runtime/options/primitive_options.py
@@ -0,0 +1,26 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2025.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Primitive options."""
+
+from .primitive_options_model import PrimitiveOptionsModel
+
+
+class PrimitiveOptions:
+    """Primitive options."""
+
+    def __init__(self, seed: int):
+        self._model = PrimitiveOptionsModel(seed=seed)
+
+    @property
+    def seed(self):
+        return self._model.seed
