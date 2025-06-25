b0d6e8d88f020fc19f179b80c12f7bc6ccac5119
Yael Ben-Haim <yaelbh@il.ibm.com>
2025-06-25 16:13:30 +0300

    fix

--- qiskit_ibm_runtime/options/primitive_options_model.py
+++ qiskit_ibm_runtime/options/primitive_options_model.py
@@ -17,27 +17,27 @@ import numpy as np
 from pydantic import BaseModel, ConfigDict
 
 
-NumArrayType = int | np.ndarray[tuple[int, ...], np.dtype[np.uint64]]
-DistributableNumType = NumArrayType | Distribute[NumArrayType]
-
-
 class Distribute(list):
     def __init__(self, *args):
         super().__init__(args)
 
     def __repr__(self):
         return f"Distribute({', '.join(map(str, self))})"
+
+
+NumArrayType = int | np.ndarray[tuple[int, ...], np.dtype[np.uint64]]
+DistributableNumType = NumArrayType | Distribute[NumArrayType]
+
+
+def get_value_for_pub_and_param(pub_index: int, param_index: NumArrayType, values_structure: DistributableNumType) -> int:
+    internal_structure = values_structure
+    if isinstance(internal_structure, Distribute):
+        internal_structure = internal_structure[pub_index]
+    if isinstance(internal_structure, np.ndarray):
+        internal_structure = internal_structure[param_index]
     
-    @staticmethod
-    def get_value_for_pub_and_param(pub_index: int, param_index: NumArrayType, values_structure: DistributableNumType) -> int:
-        internal_structure = values_structure
-        if isinstance(internal_structure, Distribute):
-            internal_structure = internal_structure[pub_index]
-        if isinstance(internal_structure, np.ndarray):
-            internal_structure = internal_structure[param_index]
-        
-        # internal_structure is certainly an integer now
-        return internal_structure
+    # internal_structure is certainly an integer now
+    return internal_structure
 
 
 class PrimitiveOptionsModel(BaseModel):
