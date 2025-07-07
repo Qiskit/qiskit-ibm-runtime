2ffb9a7399cac57c6c13eea1c90fb4bdac8164a4
joshuasn <53916441+joshuasn@users.noreply.github.com>
2025-07-07 12:47:24 -0230

    Update qiskit_ibm_runtime/options/distribute.py

--- qiskit_ibm_runtime/options/distribute.py
+++ qiskit_ibm_runtime/options/distribute.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"Distribute." ""
+"""Distribute."""
 
 from typing import Any, Generic, Iterable, Tuple, TypeVar, Union
 
