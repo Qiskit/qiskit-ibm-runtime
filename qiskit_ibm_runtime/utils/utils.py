25379c2e1dac11b3a10554b6accb4059d3ab1283
kevin-tian <kevin.tian@ibm.com>
2025-08-18 13:43:08 -0400

    Update private endpoint url

--- qiskit_ibm_runtime/utils/utils.py
+++ qiskit_ibm_runtime/utils/utils.py
@@ -337,7 +337,7 @@ def default_runtime_url_resolver(
         if private_endpoint:
             api_host = (
                 f"{parsed_url.scheme}://private.{_location_from_crn(instance)}"
-                f".quantum-computing.{parsed_url.hostname}"
+                f".quantum.{parsed_url.hostname}"
             )
         elif channel == "ibm_quantum_platform":
             # ibm_quantum_platform url
