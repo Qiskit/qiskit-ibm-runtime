62ddb2cf1c98be08cc6a8fa48f2753105cae7691
kevin-tian <kevin.tian@ibm.com>
2025-10-29 13:40:27 -0400

    Raise warning if instance limit is reached

--- qiskit_ibm_runtime/qiskit_runtime_service.py
+++ qiskit_ibm_runtime/qiskit_runtime_service.py
@@ -967,6 +967,7 @@ class QiskitRuntimeService:
             IBMRuntimeError: An error occurred running the program.
         """
 
+        self._check_instance_usage()
         qrt_options: RuntimeOptions = options
         if options is None:
             qrt_options = RuntimeOptions()
@@ -1191,6 +1192,27 @@ class QiskitRuntimeService:
             )
             usage_dict["usage_remaining_seconds"] = usage_remaining
         return usage_dict
+
+    def _check_instance_usage(self) -> None:
+        """Raise warning if instance usage has been reached."""
+        usage_dict = self._active_api_client.cloud_usage()
+        limit_reached = usage_dict.get("usage_limit_reached", False)
+        usage_remaining = usage_dict.get(
+            "usage_limit_seconds", usage_dict.get("usage_allocation_seconds")
+        ) - usage_dict.get("usage_consumed_seconds", 0)
+
+        if limit_reached:
+            if not usage_dict.get("usage_limit_seconds") or usage_remaining > 0:
+                warnings.warn(
+                    "There is currently no more time available for this instance’s plan on the account. "
+                    "Workloads will not run until time is made available. "
+                    "Check https://quantum.cloud.ibm.com/instances for more details."
+                )
+            if usage_dict.get("usage_limit_seconds") and usage_remaining <= 0:
+                warnings.warn(
+                    "This instance has met its usage limit. Workloads will not run until time is made "
+                    "available. Check https://quantum.cloud.ibm.com/instances for more details."
+                )
 
     def _decode_job(self, raw_data: Dict) -> RuntimeJobV2:
         """Decode job data received from the server.
