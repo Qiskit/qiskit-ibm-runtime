cb6d769cc2fd4da1c6a3ad183d6e572e96575da4
Yael Ben-Haim <yaelbh@il.ibm.com>
2025-12-09 12:05:01 +0200

    lint

--- test/unit/noise_learner_v3/test_noise_learner_v3.py
+++ test/unit/noise_learner_v3/test_noise_learner_v3.py
@@ -12,10 +12,10 @@
 
 """Tests the `NoiseLearnerV3` class."""
 
+from test.utils import get_mocked_backend, get_mocked_session
+
 from qiskit_ibm_runtime import Session
 from qiskit_ibm_runtime.noise_learner_v3 import NoiseLearnerV3
-
-from test.utils import get_mocked_backend, get_mocked_session
 
 from ...ibm_test_case import IBMTestCase
 
