bec62d27c085015a1f12f257dec7d29639b197d1
Javier G. Sogo <jgsogo@gmail.com>
2025-12-10 10:09:36 +0100

    A test

--- test/unit/noise_learner_v3/test_result.py
+++ test/unit/noise_learner_v3/test_result.py
@@ -17,4 +17,34 @@ class TestNoiseLearnerV3Result(IBMTestCase):
     """Tests the ``NoiseLearnerV3Result`` class."""
 
     def test_from_generators_valid_input(self):
-        self.assertEqual(1, 2)
+        """Test ``NoiseLearnerV3Result.from_generators``."""
+        generators = [
+            QubitSparsePauliList.from_label(pauli1 + pauli0)
+            for pauli1 in "IXYZ"
+            for pauli0 in "IXYZ"
+        ][1:]
+        rates = [
+            0.02452,
+            0.0,
+            0.00324,
+            0.0,
+            0.0,
+            0.0006,
+            0.0,
+            0.0,
+            0.0006,
+            0.0,
+            0.0,
+            0.0,
+            0.02452,
+            0.0,
+            0.00071,
+        ]
+        rates_std = np.arange(0, 0.15, 0.01)
+        metadata = {"learning_protocol": "lindblad"}
+        result = NoiseLearnerV3Result.from_generators(generators, rates, rates_std, metadata)
+        self.assertEqual(generators, result._generators)
+        self.assertTrue(np.array_equal(np.array(rates), result._rates))
+        self.assertTrue(np.array_equal(rates_std, result._rates_std))
+        self.assertEqual(metadata, result.metadata)
+        self.assertEqual(len(result), 15)
