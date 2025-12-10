cb50f212899a3412272de34d29242c5ee0bc7e48
ElePT <epenatap@gmail.com>
2025-12-10 17:18:57 +0100

    Fix lint

--- qiskit_ibm_runtime/transpiler/passes/basis/convert_mid_circ_meas.py
+++ qiskit_ibm_runtime/transpiler/passes/basis/convert_mid_circ_meas.py
@@ -30,9 +30,9 @@ class ConvertToMidCircuitMeasure(TransformationPass):
         custom ``measure_`` definitions. This pass is expected to run after routing, as it will check
         that ``MidCircuitMeasure`` is supported in the corresponding physical qubit.
 
-        Note that the pass will only act on non-terminal ``Measure`` instances, and won't replace existing
-        mid-circuit measurement instructions (e.g., ``"measure_2" -> "measure_3"``) or convert any
-        ``MidCircuitMeasure`` instance into a ``Measure``.
+        Note that the pass will only act on non-terminal ``Measure`` instances, and won't replace
+        existing mid-circuit measurement instructions (e.g., ``"measure_2" -> "measure_3"``) or
+        convert any ``MidCircuitMeasure`` instance into a ``Measure``.
 
         Args:
             target: Backend's target instance that contains one or more ``measure_`` instructions.
