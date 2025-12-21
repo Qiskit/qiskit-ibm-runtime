e0c6a89e6ab81038aac98548193aa3ac140cbdfa
Yael Ben-Haim <yaelbh@il.ibm.com>
2025-12-21 17:28:23 +0200

    additional tests for the decoder

--- test/unit/noise_learner_v3/test_decoder.py
+++ test/unit/noise_learner_v3/test_decoder.py
@@ -12,13 +12,12 @@
 
 """Tests the decoder for the noise learner v3 model."""
 
+import json
 import numpy as np
-from qiskit.circuit import QuantumCircuit
 from qiskit.quantum_info import QubitSparsePauliList
 
 from qiskit_ibm_runtime.noise_learner_v3.converters.version_0_1 import (
     noise_learner_v3_result_to_0_1,
-    noise_learner_v3_result_from_0_1,
 )
 from qiskit_ibm_runtime.noise_learner_v3.noise_learner_v3_decoders import (
     NoiseLearnerV3ResultDecoder,
@@ -27,7 +26,6 @@ from qiskit_ibm_runtime.noise_learner_v3.noise_learner_v3_result import (  # typ
     NoiseLearnerV3Result,
     NoiseLearnerV3Results,
 )
-from qiskit_ibm_runtime.options import NoiseLearnerV3Options
 
 from ...ibm_test_case import IBMTestCase
 
@@ -58,13 +56,31 @@ class TestDecoder(IBMTestCase):
         result1 = NoiseLearnerV3Result.from_generators(generators, rates, metadata=metadatum1)
         self.results = NoiseLearnerV3Results([result0, result1])
 
-        self.encoded = noise_learner_v3_result_to_0_1(self.results)
+        self.encoded = noise_learner_v3_result_to_0_1(self.results).model_dump_json()
 
     def test_decoder(self):
         """Tests the decoder."""
-        decoded = NoiseLearnerV3ResultDecoder.decode(self.encoded.model_dump_json())
+        decoded = NoiseLearnerV3ResultDecoder.decode(self.encoded)
         for datum_in, datum_out in zip(self.results.data, decoded.data):
             assert datum_in._generators == datum_out._generators
             assert np.allclose(datum_in._rates, datum_out._rates)
             assert np.allclose(datum_in._rates_std, datum_out._rates_std)
             assert datum_in.metadata == datum_out.metadata
+
+    def test_no_schema_version(self):
+        """Verify that an error is raised if the encoded string
+        does not specify any schema version."""
+        encoded_as_json = json.loads(self.encoded)
+        del encoded_as_json["schema_version"]
+        encoded_as_str = json.dumps(encoded_as_json)
+        with self.assertRaisesRegex(ValueError, "Missing schema version."):
+            NoiseLearnerV3ResultDecoder.decode(encoded_as_str)
+
+    def test_unknown_schema_version(self):
+        """Verify that an error is raised if the schema version specified in the encoded string
+        does not exist."""
+        encoded_as_json = json.loads(self.encoded)
+        encoded_as_json["schema_version"] = "unknown"
+        encoded_as_str = json.dumps(encoded_as_json)
+        with self.assertRaisesRegex(ValueError, "No decoder found for schema version unknown."):
+            NoiseLearnerV3ResultDecoder.decode(encoded_as_str)
