c6ffc00914c89e99864177c3485fbebfb236f6b0
Yael Ben-Haim <yaelbh@il.ibm.com>
2025-12-28 15:33:22 +0200

    creating test file for samplex item

---
+++ test/unit/quantum_program/test_samplex_item.py
@@ -0,0 +1,29 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2025.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests the ``SamplexItem`` class."""
+
+import numpy as np
+
+from qiskit.circuit import QuantumCircuit, Parameter
+
+from qiskit_ibm_runtime.quantum_program.quantum_program import SamplexItem
+
+
+from ...ibm_test_case import IBMTestCase
+
+
+class TestSamplexItem(IBMTestCase):
+    """Tests the ``SamplexItem`` class."""
+
+    def test_samplex_item(self):
+        """Test ``SamplexItem`` for a valid input."""
