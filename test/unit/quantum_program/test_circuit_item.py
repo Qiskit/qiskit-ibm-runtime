8c02d5452633189b4ac07749037610b70dfab2bc
Yael Ben-Haim <yaelbh@il.ibm.com>
2025-12-28 14:45:52 +0200

    circuit item first test

---
+++ test/unit/quantum_program/test_circuit_item.py
@@ -0,0 +1,43 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2025.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests the ``CircuitItem`` class."""
+
+import numpy as np
+
+from qiskit.circuit import QuantumCircuit, Parameter
+
+from qiskit_ibm_runtime.quantum_program.quantum_program import CircuitItem
+
+
+from ...ibm_test_case import IBMTestCase
+
+
+class TestCircuitItem(IBMTestCase):
+    """Tests the ``CircuitItem`` class."""
+
+    def test_circuit_item(self):
+        """Test ``CircuitItem`` for a valid input."""
+        circuit = QuantumCircuit(1)
+        circuit.rx(Parameter("p"), 0)
+
+        circuit_arguments = np.array([[3], [4], [5]])
+        expected_shape = (3,)
+        chunk_size = 6
+
+        circuit_item = CircuitItem(
+            circuit, circuit_arguments=circuit_arguments, chunk_size=chunk_size
+        )
+        self.assertEqual(circuit_item.circuit, circuit)
+        self.assertTrue(np.array_equal(circuit_item.circuit_arguments, circuit_arguments))
+        self.assertEqual(circuit_item.chunk_size, chunk_size)
+        self.assertEqual(circuit_item.shape, expected_shape)
