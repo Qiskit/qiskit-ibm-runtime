d6bcccb21ae6afdd893279479471cb8fde17f2ce
Yael Ben-Haim <yaelbh@il.ibm.com>
2025-12-31 12:35:30 +0200

    preparing for writing the new tests

---
+++ test/unit/quantum_program/test_result.py
@@ -0,0 +1,21 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2025.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests the class ``QuantumProgramResult``."""
+
+import numpy as np
+
+from ...ibm_test_case import IBMTestCase
+
+
+class TestQuantumProgramResult(IBMTestCase):
+    """Tests the ``QuantumProgramResult`` class."""
