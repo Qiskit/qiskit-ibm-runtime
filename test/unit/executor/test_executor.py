4943491cc57b9a09715d52eab35ff66f077a594c
Yael Ben-Haim <yaelbh@il.ibm.com>
2026-01-07 14:54:43 +0200

    Update test/unit/executor/test_executor.py
    Co-authored-by: Samuele Ferracin <sam.ferracin@ibm.com>

--- test/unit/executor/test_executor.py
+++ test/unit/executor/test_executor.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Tests the `NoiseLearnerV3` class."""
+"""Tests the `Executor` class."""
 
 from unittest.mock import patch
 
