ea4dbf974f580cb43d99f9ed669a57e5cde2db28
Yael Ben-Haim <yaelbh@il.ibm.com>
2026-01-11 17:43:06 +0200

    wrote test_to_options_model

--- test/unit/options/test_noise_learner_v3_options.py
+++ test/unit/options/test_noise_learner_v3_options.py
@@ -10,11 +10,19 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Tests the classes `NoiseLearnerV3Result` and `NoiseLearnerV3Results`."""
+"""Tests the ``NoiseLearnerV3Options`` class."""
 
+from qiskit_ibm_runtime.options.noise_learner_v3_options import NoiseLearnerV3Options
+from qiskit_ibm_runtime.options.utils import Unset
 
 from ...ibm_test_case import IBMTestCase
 
 
 class TestNoiseLearnerV3Options(IBMTestCase):
     """Tests the ``NoiseLearnerV3Options`` class."""
+
+    def test_to_options_model(self):
+        """Test the ``NoiseLearnerV3Options.to_options_model`` method."""
+        options = NoiseLearnerV3Options(num_randomizations=15, experimental={"not": "me"})
+        options_model = options.to_options_model("v0.1")
+        self.assertEqual(options_model.num_randomizations, 15)
