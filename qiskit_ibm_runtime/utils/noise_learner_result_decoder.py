af2c947d1ea42f075a566817a42b3fb70540d8e8
Yael Ben-Haim <yaelbh@il.ibm.com>
2026-01-11 15:44:39 +0200

    lint

--- qiskit_ibm_runtime/utils/noise_learner_result_decoder.py
+++ qiskit_ibm_runtime/utils/noise_learner_result_decoder.py
@@ -16,12 +16,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from qiskit_ibm_runtime.noise_learner_v3.noise_learner_v3_result import NoiseLearnerV3Results
-
-
 from .noise_learner_result import LayerError, NoiseLearnerResult, PauliLindbladError
 from .result_decoder import ResultDecoder
+
+if TYPE_CHECKING:
+    from qiskit_ibm_runtime.noise_learner_v3.noise_learner_v3_result import NoiseLearnerV3Results
 
 
 class NoiseLearnerResultDecoder(ResultDecoder):
