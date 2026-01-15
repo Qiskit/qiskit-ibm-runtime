82ff53e41f02b1a3a619065b9e576ec1b25e382c
Diego M. Rodriguez <diego@moreda.io>
2026-01-15 09:39:35 +0100

    Transpile circuits in test_noise_learner

--- test/integration/test_noise_learner.py
+++ test/integration/test_noise_learner.py
@@ -15,6 +15,7 @@
 from copy import deepcopy
 
 from qiskit.circuit import QuantumCircuit
+from qiskit.transpiler import generate_preset_pass_manager
 
 from qiskit_ibm_runtime import RuntimeJobV2, Session, EstimatorV2
 from qiskit_ibm_runtime.exceptions import IBMInputValueError
@@ -59,11 +60,9 @@ class TestIntegrationNoiseLearner(IBMIntegrationTestCase):
         options = NoiseLearnerOptions()
         learner = NoiseLearner(mode=self._backend, options=options)
 
-        try:
-            job = learner.run(self.circuits)
-        except IBMInputValueError as ex:
-            if "The instruction ecr on qubits (0, 1) is not supported" in ex.message:
-                self.skipTest("Backend does not meet requirements")
+        pm = generate_preset_pass_manager(backend=self._backend, optimization_level=0)
+        circuits = pm.run(self.circuits)
+        job = learner.run(circuits)
 
         self._verify(job, self.default_input_options, 3)
 
@@ -75,11 +74,9 @@ class TestIntegrationNoiseLearner(IBMIntegrationTestCase):
         options.layer_pair_depths = [0, 1]
         learner = NoiseLearner(mode=self._backend, options=options)
 
-        try:
-            job = learner.run(self.circuits)
-        except IBMInputValueError as ex:
-            if "The instruction ecr on qubits (0, 1) is not supported" in ex.message:
-                self.skipTest("Backend does not meet requirements")
+        pm = generate_preset_pass_manager(backend=self._backend, optimization_level=0)
+        circuits = pm.run(self.circuits)
+        job = learner.run(circuits)
 
         input_options = deepcopy(self.default_input_options)
         input_options["max_layers_to_learn"] = 1
@@ -93,11 +90,9 @@ class TestIntegrationNoiseLearner(IBMIntegrationTestCase):
         options.max_layers_to_learn = 0
         learner = NoiseLearner(mode=self._backend, options=options)
 
-        try:
-            job = learner.run(self.circuits)
-        except IBMInputValueError as ex:
-            if "The instruction ecr on qubits (0, 1) is not supported" in ex.message:
-                self.skipTest("Backend does not meet requirements")
+        pm = generate_preset_pass_manager(backend=self._backend, optimization_level=0)
+        circuits = pm.run(self.circuits)
+        job = learner.run(circuits)
 
         self.assertEqual(job.result().data, [])
 
@@ -154,7 +149,7 @@ class TestIntegrationNoiseLearner(IBMIntegrationTestCase):
         job.wait_for_final_state()
 
         result = job.result()
-        self.assertEqual(len(result), n_results)
+        self.assertGreaterEqual(len(result), n_results)
 
         for datum in result.data:
             circuit = datum.circuit
