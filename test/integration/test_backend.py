0edd8c3fed3e42005de61314238ca6b5a8745df2
Janani Ananthanarayanan <101062130+jan-an@users.noreply.github.com>
2026-01-19 19:27:00 +0530

    Correct fractional gates configuration check for cached usage of backend object (#2547)
    * fix condition
    
    * release notes
    
    * Test to check if cache is used correctly when backends with fractional gates are called again
    
    * formatting fixes
    
    * remove sanity check line since it feels a bit unnecessary
    
    * formatting change again
    
    * fix condition
    
    * Update release-notes/unreleased/2524.bug.rst
    
    Added fullstop
    
    Co-authored-by: Yael Ben-Haim <yaelbh@il.ibm.com>
    
    * Include history of the issue
    
    Included the issue pertaining to the bug in addition to information about the fix in release notes
    
    * Update test_backend.py
    
    Added a few more assertions to round off issues we saw in the bug report
    
    * style and variable name fix
    
    * change error message and delete assert
    
    * style change
    
    * edit release notes
    
    * edit release notes
    
    * edit test
    
    * empty1
    
    * empty2
    
    * Update release-notes/unreleased/2524.bug.rst
    
    Co-authored-by: Yael Ben-Haim <yaelbh@il.ibm.com>
    
    ---------
    
    Co-authored-by: Yael Ben-Haim <yaelbh@il.ibm.com>

--- test/integration/test_backend.py
+++ test/integration/test_backend.py
@@ -276,6 +276,36 @@ class TestIBMBackend(IBMIntegrationTestCase):
             ):
                 self.service.backend(backend.name, use_fractional_gates=True)
 
+    def test_backend_fractional_gates_cache_behavior(self):
+        """Test backend config cache/refresh logic for use_fractional_gates."""
+        try:
+            real_device_name = "ibm_miami"
+            backend_fg = self.service.backend(real_device_name, use_fractional_gates=True)
+            backend_fg2 = self.service.backend(real_device_name, use_fractional_gates=True)
+            backend_no_fg = self.service.backend(real_device_name, use_fractional_gates=False)
+            backend_no_fg2 = self.service.backend(real_device_name, use_fractional_gates=False)
+            backend_fg3 = self.service.backend(real_device_name, use_fractional_gates=True)
+        except QiskitBackendNotFoundError:
+            self.skipTest("Real backend not available.")
+
+        self.assertIs(
+            backend_fg, backend_fg2, "Cache was not used for repeated use_fractional_gates=True"
+        )
+
+        self.assertIsNot(
+            backend_fg,
+            backend_no_fg,
+            "Configuration was not refreshed when use_fractional_gates changed",
+        )
+
+        self.assertIs(backend_no_fg, backend_no_fg2, "Cache was not used to create backend object")
+
+        self.assertIsNot(
+            backend_no_fg2,
+            backend_fg3,
+            "Configuration was not refreshed when use_fractional_gates changed",
+        )
+
     def test_renew_backend_properties(self):
         """Test renewed backend property"""
         name = self.backend.name
