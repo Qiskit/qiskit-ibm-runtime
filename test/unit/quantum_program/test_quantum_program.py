c10c532c0ac5b24437688c57f33290bdac8f4f60
Yael Ben-Haim <yaelbh@il.ibm.com>
2026-01-29 16:11:38 +0200

    reproduced in a test

--- test/unit/quantum_program/test_quantum_program.py
+++ test/unit/quantum_program/test_quantum_program.py
@@ -141,3 +141,37 @@ class TestQuantumProgram(IBMTestCase):
             quantum_program.append(
                 template_circuit, samplex=samplex, circuit_arguments=parameter_values
             )
+    
+    def test_subset_of_noise_maps(self):
+        """Test that `QuantumProgram` knows to handle the case where a specific samplex
+        item uses only a subset of the program's noise maps."""
+        shots = 100
+
+        noise_models = [
+            PauliLindbladMap.from_list([("IX", 0.04), ("XX", 0.05)]),
+            PauliLindbladMap.from_list([("XI", 0.02), ("IZ", 0.035)]),
+        ]
+
+        quantum_program = QuantumProgram(
+            shots=shots,
+            noise_maps={f"pl{i}": noise_model for i, noise_model in enumerate(noise_models)},
+        )
+
+        circuit2 = QuantumCircuit(2)
+        with circuit2.box(annotations=[Twirl(), InjectNoise(ref="pl1")]):
+            circuit2.measure_all()
+
+        template_circuit, samplex = build(circuit2)
+        quantum_program.append(
+            template_circuit,
+            samplex=samplex,
+            shape=(4, 3, 2),
+            chunk_size=7,
+        )
+
+        samplex_item = quantum_program.items[0]
+        self.assertEqual(samplex_item.samplex, samplex)
+        self.assertEqual(samplex_item.circuit, template_circuit)
+        self.assertEqual(samplex_item.chunk_size, 7)
+        self.assertEqual(samplex_item.shape, (4, 3, 2))
+        self.assertEqual(samplex_item.samplex_arguments[f"pauli_lindblad_maps.pl1"], noise_models[1])
