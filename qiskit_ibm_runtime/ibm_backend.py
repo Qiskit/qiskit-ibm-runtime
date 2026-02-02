f486f218abb8f248aadeeeac9a7c2976f3bfbb80
SamFerracin <sam.ferracin@ibm.com>
2026-02-01 19:54:05 -0500

    switch

--- qiskit_ibm_runtime/ibm_backend.py
+++ qiskit_ibm_runtime/ibm_backend.py
@@ -44,7 +44,7 @@ from .models import (
 )
 from .options.executor_options import ExecutorOptions
 from .quantum_program import QuantumProgram
-from .quantum_program.converters import quantum_program_to_0_1, quantum_program_result_from_0_1
+from .quantum_program.converters import quantum_program_to_0_2_dev, quantum_program_result_from_0_2_dev
 from .runtime_job_v2 import RuntimeJobV2
 from .utils import local_to_utc
 from .utils.backend_converter import convert_to_target
@@ -249,7 +249,7 @@ class IBMBackend(Backend):
         """
         options = options or ExecutorOptions()
         program_id = "executor"
-        model = quantum_program_to_0_1(program, options)
+        model = quantum_program_to_0_2_dev(program, options)
 
         params = model.model_dump()
         params["version"] = 2  # TODO: this is a work-around for the dispatch while we use 'execute'
@@ -273,7 +273,7 @@ class IBMBackend(Backend):
             def decode(cls, data: str):  # type: ignore[no-untyped-def]
                 """Decode."""
                 obj = QuantumProgramResultModel.model_validate_json(data)
-                return quantum_program_result_from_0_1(obj)
+                return quantum_program_result_from_0_2_dev(obj)
 
         return RuntimeJobV2(
             backend=self,
