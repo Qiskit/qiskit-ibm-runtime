bee972c0ac39d33334e8d28c0728813d177bcb34
Diego M. Rodriguez <diego@moreda.io>
2026-02-26 18:48:36 +0100

    Split test_circuit_on_fake_backend_v2

--- test/unit/test_fake_backends.py
+++ test/unit/test_fake_backends.py
@@ -13,10 +13,9 @@
 # pylint: disable=missing-class-docstring,missing-function-docstring
 # pylint: disable=missing-module-docstring
 
-import itertools
 import operator
 
-from ddt import ddt, data, idata, unpack
+from ddt import ddt, data
 
 from qiskit.circuit import QuantumCircuit
 from qiskit.compiler import transpile
@@ -55,16 +54,25 @@ class TestFakeBackends(IBMTestCase):
         cls.circuit.x(1)
         cls.circuit.measure_all()
 
-    @idata(
-        itertools.product(
-            [be for be in FAKE_PROVIDER_FOR_BACKEND_V2.backends() if be.num_qubits > 1],
-            [0, 1, 2, 3],
-        )
-    )
-    @unpack
-    def test_circuit_on_fake_backend_v2(self, backend, optimization_level):
+    @data(*[be for be in FAKE_PROVIDER_FOR_BACKEND_V2.backends() if be.num_qubits > 1])
+    def test_circuit_on_fake_backend_v2(self, backend):
+        """Test running a circuit in fake backends."""
         if not optionals.HAS_AER and backend.num_qubits > 20:
             self.skipTest(f"Unable to run fake_backend {backend.backend_name} without qiskit-aer")
+        backend.set_options(seed_simulator=42)
+        pm = generate_preset_pass_manager(backend=backend, optimization_level=0)
+        isa_circuit = pm.run(self.circuit)
+        sampler = Sampler(backend)
+        job = sampler.run([isa_circuit])
+        pub_result = job.result()[0]
+        counts = pub_result.data.meas.get_counts()
+        max_count = max(counts.items(), key=operator.itemgetter(1))[0]
+        self.assertEqual(max_count, "11")
+
+    @data(0, 1, 2, 3)
+    def test_circuit_on_fake_backend_v2_with_optimization_level(self, optimization_level):
+        """Test running a circuit in a fake backend with different optimization level."""
+        backend = FakeSherbrooke()
         backend.set_options(seed_simulator=42)
         pm = generate_preset_pass_manager(backend=backend, optimization_level=optimization_level)
         isa_circuit = pm.run(self.circuit)
