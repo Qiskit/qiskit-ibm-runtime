39f1df137d54b237329c517689818f3c35d2fc3b
SamFerracin <sam.ferracin@ibm.com>
2026-03-05 15:44:51 -0500

    oops

--- test/integration/executor/routines/test_sampler_v2.py
+++ test/integration/executor/routines/test_sampler_v2.py
@@ -24,7 +24,6 @@ from qiskit.primitives.containers.data_bin import DataBin
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 
 from qiskit_ibm_runtime.executor.routines.sampler_v2 import SamplerV2, SamplerOptions
-from qiskit_ibm_runtime.fake_provider import FakeManilaV2
 from ....ibm_test_case import IBMIntegrationTestCase
 
 
