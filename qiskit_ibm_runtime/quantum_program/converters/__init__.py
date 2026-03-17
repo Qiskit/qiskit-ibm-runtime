f06bcca94b55231de041e69b56b4732ef4cd4289
SamFerracin <sam.ferracin@ibm.com>
2026-03-17 15:57:10 -0400

    done

--- qiskit_ibm_runtime/quantum_program/converters/__init__.py
+++ qiskit_ibm_runtime/quantum_program/converters/__init__.py
@@ -12,5 +12,13 @@
 
 """Converters for executor."""
 
-from .converters_0_1 import quantum_program_to_0_1, quantum_program_result_from_0_1
-from .converters_0_2 import quantum_program_result_from_0_2, quantum_program_to_0_2
+from .converters_0_1 import (
+    quantum_program_to_0_1,
+    quantum_program_from_0_1,
+    quantum_program_result_from_0_1,
+)
+from .converters_0_2 import (
+    quantum_program_to_0_2,
+    quantum_program_from_0_2,
+    quantum_program_result_from_0_2,
+)
--- qiskit_ibm_runtime/quantum_program/converters/converters_0_2.py
+++ qiskit_ibm_runtime/quantum_program/converters/converters_0_2.py
@@ -35,7 +35,59 @@ from ...utils.utils import get_qpy_version, get_ssv_version
 
 from ..quantum_program import QuantumProgram, CircuitItem, SamplexItem
 from ..quantum_program_result import QuantumProgramResult, ChunkPart, ChunkSpan, Metadata
-from ...options.executor_options import ExecutorOptions
+from ...options.executor_options import ExecutorOptions, ExecutionOptions
+
+
+def quantum_program_from_0_2(model: ParamsModel) -> tuple[QuantumProgram, ExecutionOptions]:
+    """Convert a V0.2 model to a pair of program and options."""
+    program_model = model.quantum_program
+    items: list[CircuitItem | SamplexItem] = []
+    for model_item in program_model.items:
+        chunk_size = None if model_item.chunk_size == "auto" else model_item.chunk_size
+
+        if model_item.item_type == "circuit":
+            circuit = model_item.circuit.to_quantum_circuit(use_cached=True)
+            circuit_arguments = model_item.circuit_arguments.to_numpy()
+
+            items.append(
+                CircuitItem(
+                    circuit=circuit,
+                    circuit_arguments=circuit_arguments,
+                    chunk_size=chunk_size,
+                )
+            )
+        elif model_item.item_type == "samplex":
+            circuit = model_item.circuit.to_quantum_circuit(use_cached=True)
+            samplex = model_item.samplex.to_samplex(use_cached=True)
+
+            samplex_arguments = samplex.inputs().make_broadcastable()
+            for name, value in model_item.samplex_arguments.items():
+                if isinstance(value, TensorModel):
+                    samplex_arguments[name] = value.to_numpy()
+                elif isinstance(value, PauliLindbladMapModel):
+                    samplex_arguments[name] = value.to_pauli_lindblad_map()
+                else:
+                    samplex_arguments[name] = value
+
+            items.append(
+                SamplexItem(
+                    circuit=circuit,
+                    samplex=samplex,
+                    samplex_arguments=samplex_arguments,
+                    chunk_size=chunk_size,
+                    shape=tuple(model_item.shape),
+                )
+            )
+        else:
+            raise ValueError("Unexpected model item type.")
+
+    quantum_program = QuantumProgram(shots=program_model.shots, items=items)
+
+    options = ExecutionOptions()
+    options.init_qubits = model.options.init_qubits
+    options.rep_delay = model.options.rep_delay
+
+    return quantum_program, options
 
 
 def quantum_program_to_0_2(program: QuantumProgram, options: ExecutorOptions) -> ParamsModel:
