d7d811b5f7f1a68802a5295443e455de1de0958a
dekelmeirom <33314493+dekelmeirom@users.noreply.github.com>
2026-06-02 09:45:31 +0300

    add trex mitigation support for wrapper estimator (#2839)
    * add trex mitigation support for wrapper estimator
    
    * fix lint
    
    * add tests to trex_utils
    
    * Apply suggestions from code review
    
    Co-authored-by: Samuele Ferracin <sam.ferracin@ibm.com>
    
    * add measure_noise_learning.shots_per_randomization
    
    * combine measure_noise_learning and measure_mitigation
    
    * move post-processing trex_utils to decoders
    
    * fix trex scale factor calculation
    
    * change trex_noise_model function name
    
    ---------
    
    Co-authored-by: Samuele Ferracin <sam.ferracin@ibm.com>

---
+++ qiskit_ibm_runtime/decoders/executor_estimator/trex_utils.py
@@ -0,0 +1,69 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Helper functions for the Twirled Readout Error eXtinction (TREX) post-processing."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..results.quantum_program import QuantumProgramItemResult
+
+import numpy as np
+from qiskit.quantum_info import Pauli, PauliLindbladMap, QubitSparsePauli
+
+
+def get_processed_calibration_data(calibration_result: QuantumProgramItemResult) -> np.ndarray:
+    """Process data from TREX calibration circuit results.
+
+    Args:
+        calibration_result: QuantumProgramItemResult of the TREX calibration circuit.
+
+    Returns:
+        TREX flipped calibration data.
+    """
+    if "_trex_cal" not in calibration_result:
+        raise ValueError("Dedicated TREX calibration circuit is missing from the results.")
+
+    trex_noise_calibration_data = calibration_result["_trex_cal"]
+    trex_calibration_measurement_flips = calibration_result["measurement_flips._trex_cal"]
+    return np.logical_xor(trex_noise_calibration_data, trex_calibration_measurement_flips)
+
+
+def calculate_trex_factor(
+    noise_data: PauliLindbladMap | np.ndarray, observable_term: Pauli | str
+) -> float:
+    """Calculate TREX factor relevant for a given observable term based on noise model.
+
+    Args:
+        noise_data: PauliLindbladMap containing measurement noise model or a result of TREX
+            calibration execution.
+        observable_term: observable term to calculate TREX factor for.
+
+    Returns:
+        TREX factor for the observable term.
+    """
+    sparse_pauli = QubitSparsePauli(observable_term)
+    if isinstance(noise_data, PauliLindbladMap):
+        z_sparse_pauli = QubitSparsePauli(
+            ("Z" * len(sparse_pauli.indices), sparse_pauli.indices),
+            num_qubits=sparse_pauli.num_qubits,
+        )
+        return 1 / noise_data.pauli_fidelity(z_sparse_pauli)
+    # The input is a result of TREX calibration execution
+    # treat every non identity Pauli as Z
+    evals = np.prod(1 - 2 * noise_data[..., sparse_pauli.indices], axis=-1)
+    shots = noise_data.shape[0] * noise_data.shape[-2]  # randomizations * shots_per_randomizations
+
+    # Compute trex factor
+    return 1 / (np.sum(evals) / shots)
---
+++ test/unit/executor_estimator/test_estimator_v2_trex_utils.py
@@ -0,0 +1,79 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Unit tests for EstimatorV2 TREX helper functions."""
+
+import unittest
+
+from qiskit import QuantumCircuit
+from qiskit.primitives.containers.estimator_pub import EstimatorPub
+from qiskit.quantum_info import SparsePauliOp
+
+from qiskit_ibm_runtime.executor_estimator.trex_utils import (
+    create_trex_calibration_circuit,
+)
+from qiskit_ibm_runtime.options_models.measure_noise_learning_options import (
+    MeasureNoiseLearningOptions,
+)
+from qiskit_ibm_runtime.quantum_program.quantum_program import SamplexItem
+
+
+class TestCreateTrexCalibrationCircuit(unittest.TestCase):
+    """Tests for create_trex_calibration_circuit function."""
+
+    def test_creates_samplex_item_with_max_qubits_and_requested_randomizations(self):
+        """Test calibration circuit shape and size are derived from inputs."""
+        circuit1 = QuantumCircuit(2)
+        circuit1.h(0)
+        circuit1.cx(0, 1)
+
+        circuit2 = QuantumCircuit(3)
+        circuit2.x(0)
+        circuit2.cx(0, 1)
+        circuit2.cx(1, 2)
+
+        pub1 = EstimatorPub.coerce((circuit1, SparsePauliOp.from_list([("ZZ", 1)])))
+        pub2 = EstimatorPub.coerce((circuit2, SparsePauliOp.from_list([("ZZZ", 1)])))
+
+        measure_noise_learning = MeasureNoiseLearningOptions()
+        measure_noise_learning.num_randomizations = 16
+
+        result = create_trex_calibration_circuit([pub1, pub2], measure_noise_learning)
+
+        self.assertIsInstance(result, SamplexItem)
+        self.assertEqual(result.shape, (16,))
+        self.assertEqual(result.circuit.num_qubits, 3)
+        self.assertIn("_trex_cal", result.circuit.cregs[0].name)
+
+    def test_uses_default_randomizations_when_not_integer(self):
+        """Test default randomizations value is used when option is not an integer."""
+        circuit = QuantumCircuit(2)
+        pub = EstimatorPub.coerce((circuit, SparsePauliOp.from_list([("ZZ", 1)])))
+
+        measure_noise_learning = MeasureNoiseLearningOptions()
+
+        result = create_trex_calibration_circuit([pub], measure_noise_learning)
+
+        self.assertEqual(result.shape, (32,))
+
+    def test_creates_measurement_only_calibration_circuit(self):
+        """Test generated TREX circuit contains measurements and no state-preparation gates."""
+        circuit = QuantumCircuit(2)
+        circuit.h(0)
+        circuit.cx(0, 1)
+        pub = EstimatorPub.coerce((circuit, SparsePauliOp.from_list([("ZZ", 1)])))
+
+        result = create_trex_calibration_circuit([pub], MeasureNoiseLearningOptions())
+
+        operation_names = {instruction.operation.name for instruction in result.circuit.data}
+        self.assertIn("measure", operation_names)
+        self.assertFalse({"h", "x", "cx"} & operation_names)
