4f9f7c447dd48edf62d0715be1fd4c14cda2cf81
Tsafrir Armon <tsafrir.armon@ibm.com>
2026-06-04 14:41:26 +0300

    add resilience to options

--- qiskit_ibm_runtime/options_models/estimator_options.py
+++ qiskit_ibm_runtime/options_models/estimator_options.py
@@ -23,6 +23,7 @@ from qiskit_ibm_runtime.options_models.execution_options import ExecutionOptions
 from qiskit_ibm_runtime.options_models.executor_options import ExecutorOptions
 
 from .environment_options import EnvironmentOptions
+from .resilience_options import ResilienceOptionsV2
 from .twirling_options import TwirlingOptions
 from .utils import PRIMITIVES_CONFIG
 
@@ -80,6 +81,12 @@ class EstimatorOptions:
 
     environment: EnvironmentOptions = Field(default_factory=EnvironmentOptions)
     """Options related to the execution environment."""
+
+    resilience: ResilienceOptionsV2 = Field(default_factory=ResilienceOptionsV2)
+    """Advanced resilience options to fine-tune the resilience strategy.
+
+    See :class:`ResilienceOptionsV2` for all available options.
+    """
 
     def to_executor_options(self) -> ExecutorOptions:
         """Map EstimatorOptions to ExecutorOptions, ignoring all irrelevant fields.
---
+++ qiskit_ibm_runtime/options_models/resilience_options.py
@@ -0,0 +1,42 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Resilience options."""
+
+from __future__ import annotations
+
+from pydantic import Field
+from pydantic.dataclasses import dataclass
+
+from .measure_noise_learning_options import MeasureNoiseLearningOptions
+from .utils import PRIMITIVES_CONFIG
+
+
+@dataclass(config=PRIMITIVES_CONFIG)
+class ResilienceOptionsV2:
+    """Resilience options for V2 Estimator."""
+
+    measure_mitigation: bool = True
+    """Whether to enable measurement error mitigation method.
+
+    If you enable measurement mitigation, you can fine-tune its noise learning
+    by using :attr:`~measure_noise_learning`. See :class:`MeasureNoiseLearningOptions`
+    for all measurement mitigation noise learning options.
+    """
+    measure_noise_learning: MeasureNoiseLearningOptions = Field(
+        default_factory=MeasureNoiseLearningOptions
+    )
+
+    """Additional measurement noise learning options.
+
+    See :class:`MeasureNoiseLearningOptions` for all options.
+    """
