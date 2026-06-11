c9c3ca586d328f48e8b6ac49f923481bfeaa89c1
DEKEL MEIROM <Dekel.Meirom@ibm.com>
2026-06-11 19:44:10 +0300

    pea prepare

---
+++ qiskit_ibm_runtime/executor_estimator/pea_utils.py
@@ -0,0 +1,180 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Helper functions for the PEC error mitigation method."""
+
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from qiskit import QuantumCircuit
+    from qiskit.primitives.containers.estimator_pub import EstimatorPub
+    from qiskit.quantum_info import PauliLindbladMap
+
+    from ..options_models.measure_noise_learning_options import MeasureNoiseLearningOptions
+    from ..options_models.zne_options import ZneOptions
+    from ..options_models.twirling_options import TwirlingOptions
+
+import numpy as np
+from samplomatic import InjectNoise, build
+from samplomatic.utils import get_annotation
+
+from ..exceptions import IBMInputValueError
+from ..executor.calculate_twirling_shots import calculate_twirling_shots
+from ..executor.passthrough_utils import validate_and_extract_metadata
+from ..quantum_program import QuantumProgram
+from ..quantum_program.quantum_program import SamplexItem
+from .prepare import box_circuit, compute_samplex_arguments, make_samplex_arguments
+from .trex_utils import create_trex_calibration_circuit
+
+logger = logging.getLogger(__name__)
+
+def prepare_pea(
+    pubs: Sequence[EstimatorPub],
+    twirling_options: TwirlingOptions,
+    shots: int,
+    zne_options: ZneOptions,
+    noise_model_mapping: Sequence[dict[str, PauliLindbladMap]],
+    measure_noise_learning: MeasureNoiseLearningOptions | None = None,
+) -> QuantumProgram:
+    """Convert estimator PUBs to a quantum program.
+
+    Args:
+        pubs: List of estimator pubs to convert.
+        twirling_options: The twirling options.
+        shots: The number of shots to use. Will be overridden by
+            ``num_randomizations * shots_per_randomization`` when both are specified explicitly
+            and twirling is on.
+        measure_noise_learning: The measure noise learning options. If provided, Twirled Readout
+            Error eXtinction (TREX) mitigation method will be used.
+        zne_options: The options for PEA mitigation.
+        noise_model_mapping: List of mapping between layer ref to a noise model to use for noise
+            amplification. The list must contain a map for each pub. Assumes that the
+            unique layers used for noise learning were extracted using the ``get_layers`` method.
+
+    Returns:
+        :class:`~.QuantumProgram` with :class:`~.SamplexItem` objects for each pub,
+        with ``passthrough data`` configured for
+        :class:`~qiskit_ibm_runtime.executor_estimator.estimator.EstimatorV2` post-processing.
+
+    Raises:
+        IBMInputValueError: If pubs have mismatched precision,
+            if a circuit contains mid-circuit measurements, or if a circuit already uses the
+            reserved classical register name ``_meas``.
+        IBMInputValueError: If the length of noise_model_mapping and the length of the pubs
+            mismatched.
+
+    """
+    num_randomizations, shots_per_randomization = calculate_twirling_shots(
+        shots,
+        twirling_options.num_randomizations,
+        twirling_options.shots_per_randomization,
+    )
+
+    # validate noise_model_mapping length
+    if noise_model_mapping is None or (
+            noise_model_mapping is not None and len(noise_model_mapping) != len(pubs)
+    ):
+        raise IBMInputValueError(
+            "If PEA mitigation is used, the input must contain noise_model_mapping for each pub"
+        )
+
+    # Create items
+    items: list[SamplexItem] = []
+    observables_list = []
+    param_basis_pairs_list = []
+    param_shapes_list = []
+
+    for i, pub in enumerate(pubs):
+        logger.info("Processing pub %d/%d", i + 1, len(pubs))
+
+        boxed_circuit = box_circuit(
+            pub.circuit, twirling_options, measure_noise_learning is not None, True
+        )
+
+        # Build the template and the samplex
+        template, samplex = build(boxed_circuit)
+
+        # Prepare samplex_arguments
+        flat_parameter_values, change_basis, param_basis_pairs = compute_samplex_arguments(pub)
+        samplex_arguments = make_samplex_arguments(
+            samplex, boxed_circuit, flat_parameter_values, change_basis
+        )
+
+        # add samplex_arguments related to noise injection
+        if not isinstance(zne_options.noise_factors, Sequence) and not isinstance(zne_options.noise_factors[0], (float, int)):
+            raise IBMInputValueError("ZNE noise factors option must be a sequence of floats or ints")
+        noise_factors = np.array(zne_options.noise_factors)
+        # Removing from the noise_factors 1 which is represented by the noise of the data gates layer
+        noise_scales = noise_factors - 1
+        for ref in noise_model_mapping[i]:
+            samplex_arguments[f"noise_scales.{ref}"] = noise_scales
+        samplex_arguments["pauli_lindblad_maps"] = noise_model_mapping[i]
+
+        # Create SamplexItem
+        shape = (num_randomizations, change_basis.shape[0])
+        items.append(
+            SamplexItem(
+                circuit=template,
+                samplex=samplex,
+                samplex_arguments=samplex_arguments,
+                shape=shape,
+            )
+        )
+
+        # Store data for passthrough
+        observables_list.append(pub.observables.tolist())
+        param_basis_pairs_list.append(param_basis_pairs)
+        param_shapes_list.append(pub.parameter_values.shape)
+
+    # Collect and validate circuit metadata from each pub
+    circuits_metadata = validate_and_extract_metadata(pubs)
+
+    passthrough_data = {
+        "post_processor": {
+            "version": "v0.1",
+            "circuits_metadata": circuits_metadata,
+            "observables": observables_list,
+            "param_basis_pairs": param_basis_pairs_list,
+            "param_shapes": param_shapes_list,
+            "measure_mitigation": "False",
+        },
+    }
+
+    # Create QuantumProgram
+    quantum_program = QuantumProgram(
+        shots=shots_per_randomization,
+        items=items,
+        passthrough_data=passthrough_data,
+    )
+
+    # Add TREX calibration circuit
+    if measure_noise_learning is not None:
+        if (
+            isinstance(measure_noise_learning.shots_per_randomization, int)
+            and measure_noise_learning.shots_per_randomization != shots_per_randomization
+        ):
+            raise IBMInputValueError(
+                "shots_per_randomization must be the same for twirling and measure_noise_learning"
+            )
+        trex_item = create_trex_calibration_circuit(pubs, measure_noise_learning)
+        quantum_program.items.append(trex_item)
+        passthrough_data["post_processor"]["measure_mitigation"] = "True"
+
+    # Set semantic role for post-processing dispatch
+    quantum_program._semantic_role = "estimator_v2"
+
+    return quantum_program
