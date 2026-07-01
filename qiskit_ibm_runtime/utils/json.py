4465d985b7e9d750e09eb2e07ba59bf5d597db4f
Diego M. Rodriguez <diego@moreda.io>
2026-07-01 16:59:56 +0200

    Lint and typos

--- qiskit_ibm_runtime/utils/json.py
+++ qiskit_ibm_runtime/utils/json.py
@@ -15,23 +15,23 @@
 import warnings
 
 from qiskit_ibm_runtime.json import (  # noqa: F401
-    SERVICE_MAX_SUPPORTED_QPY_VERSION,
-    LEGACY_NOISE_LEARNER_MODULE,
     CURRENT_NOISE_LEARNER_MODULE,
-    to_base64_string,
-    _serialize_and_encode,
+    LEGACY_NOISE_LEARNER_MODULE,
+    SERVICE_MAX_SUPPORTED_QPY_VERSION,
+    RuntimeDecoder,
+    RuntimeEncoder,
+    _cast_strings_keys_to_int,
     _decode_and_deserialize,
     _deserialize_from_json,
     _deserialize_from_settings,
+    _serialize_and_encode,
     _set_int_keys_flag,
-    _cast_strings_keys_to_int,
-    RuntimeDecoder,
-    RuntimeEncoder,
+    to_base64_string,
 )
 
 warnings.warn(
     "The `qiskit_ibm_runtime.utils.json` package has been moved to `qiskit_ibm_runtime.json` as "
-    "of qiskit_ibm_runtime v0.48.0, and it will ne its only location in a future release, 3 months "
+    "of qiskit_ibm_runtime v0.48.0, and it will be its only location in a future release, 3 months "
     "or more after v0.48.0. Please adjust your imports accordingly, and note that the only public "
     "items are `RuntimeDecoder` and `RuntimeEncoder`, which are available as top-level imports.",
     DeprecationWarning,
