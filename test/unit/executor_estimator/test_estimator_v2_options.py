867d7a088abd1e91d5293958bb036c17bbd5408c
Diego M. Rodriguez <diego@moreda.io>
2026-07-17 12:21:57 +0200

    Adjust tests

--- test/unit/executor_estimator/test_estimator_v2_options.py
+++ test/unit/executor_estimator/test_estimator_v2_options.py
@@ -16,77 +16,11 @@ from pydantic import ValidationError
 
 from qiskit_ibm_runtime.executor_estimator.estimator import EstimatorV2
 from qiskit_ibm_runtime.fake_provider import FakeBrisbane
-from qiskit_ibm_runtime.options_models.dynamical_decoupling_options import (
-    DynamicalDecouplingOptions,
-)
 from qiskit_ibm_runtime.options_models.environment_options import EnvironmentOptions
 from qiskit_ibm_runtime.options_models.estimator_options import EstimatorOptions
 from qiskit_ibm_runtime.options_models.execution_options import ExecutionOptions
-from qiskit_ibm_runtime.options_models.executor_options import ExecutorOptions
 
 from ...ibm_test_case import IBMTestCase
-
-
-class TestEstimatorOptions(IBMTestCase):
-    """Tests for EstimatorOptions."""
-
-    def test_default_values(self):
-        """Test default values."""
-        options = EstimatorOptions()
-        self.assertEqual(options.default_precision, 0.015625)
-        self.assertIsInstance(options.dynamical_decoupling, DynamicalDecouplingOptions)
-        self.assertIsInstance(options.execution, ExecutionOptions)
-        self.assertEqual(options.experimental, {})
-        self.assertIsNone(options.max_execution_time)
-        self.assertIsInstance(options.environment, EnvironmentOptions)
-        self.assertIsNone(options.resilience.measure_mitigation)
-        self.assertEqual(options.resilience.measure_noise_learning.num_randomizations, "auto")
-
-    def test_set_default_precision(self):
-        """Test setting default_precision."""
-        options = EstimatorOptions(default_precision=0.01)
-        self.assertEqual(options.default_precision, 0.01)
-
-    def test_set_execution_options(self):
-        """Test setting execution options."""
-        exec_options = ExecutionOptions(init_qubits=True)
-        options = EstimatorOptions(execution=exec_options)
-        self.assertTrue(options.execution.init_qubits)
-
-    def test_set_max_execution_time(self):
-        """Test setting max_execution_time."""
-        options = EstimatorOptions(max_execution_time=300)
-        self.assertEqual(options.max_execution_time, 300)
-
-    def test_set_experimental(self):
-        """Test setting experimental options."""
-        options = EstimatorOptions(experimental={"test_option": "value"})
-        self.assertEqual(options.experimental, {"test_option": "value"})
-
-    def test_to_executor_options(self):
-        """Test conversion to ExecutorOptions."""
-        options = EstimatorOptions(
-            default_precision=0.022097,
-            max_execution_time=300,
-        )
-        options.execution.init_qubits = True
-        options.execution.rep_delay = 0.001
-
-        executor_options = options.to_executor_options()
-
-        self.assertIsInstance(executor_options, ExecutorOptions)
-        self.assertTrue(executor_options.execution.init_qubits)
-        self.assertEqual(executor_options.execution.rep_delay, 0.001)
-        self.assertEqual(executor_options.environment.max_execution_time, 300)
-
-    def test_to_executor_options_with_experimental(self):
-        """Test conversion with experimental options."""
-        options = EstimatorOptions(experimental={"image": "custom:image", "other": "value"})
-
-        executor_options = options.to_executor_options()
-
-        self.assertEqual(executor_options.environment.image, "custom:image")
-        self.assertEqual(executor_options.experimental.get("other"), "value")
 
 
 class TestEstimatorUsingOptions(IBMTestCase):
--- test/unit/options_models/test_sampler.py
+++ test/unit/options_models/test_sampler.py
@@ -18,8 +18,10 @@ from ddt import data, ddt
 from pydantic import ValidationError
 from qiskit.transpiler import CouplingMap
 
-from qiskit_ibm_runtime.options_models.sampler import SamplerOptions
-from qiskit_ibm_runtime.options_models.simulator import SimulatorOptions
+from qiskit_ibm_runtime.executor_sampler import SamplerV2
+from qiskit_ibm_runtime.fake_provider import FakeBrisbane
+from qiskit_ibm_runtime.options_models.sampler_options import SamplerOptions
+from qiskit_ibm_runtime.options_models.simulator_options import SimulatorOptions
 
 from ...ibm_test_case import IBMTestCase
 
@@ -37,13 +39,36 @@ class TestSimulatorOptions(IBMTestCase):
         self.assertIsNone(options.simulator.coupling_map)
         self.assertIsNone(options.simulator.basis_gates)
 
-    @data([[0, 1], [1, 2]], CouplingMap([[0, 1], [1, 2]]))
-    def test_coupling_map_valid(self, coupling_map):
-        """Test setting coupling map."""
+    def test_simulator_options_set_coupling_map(self):
+        """Test setting coupling map as list."""
         options = SamplerOptions()
+        coupling_map = [[0, 1], [1, 2], [2, 3]]
         options.simulator.coupling_map = coupling_map
 
-        self.assertEqual(options.simulator.coupling_map, coupling_map)
+        self.assertEqual(options.simulator.coupling_map, CouplingMap(coupling_map))
+
+    def test_simulator_options_set_coupling_map_from_qiskit(self):
+        """Test setting coupling map from Qiskit CouplingMap object."""
+        options = SamplerOptions()
+        qiskit_coupling_map = CouplingMap([[0, 1], [1, 2]])
+        options.simulator.coupling_map = qiskit_coupling_map
+
+        self.assertEqual(options.simulator.coupling_map, qiskit_coupling_map)
+
+    def test_simulator_options_from_dict(self):
+        """Test constructing simulator options from dict."""
+        opts_dict = {
+            "simulator": {
+                "seed_simulator": 123,
+                "basis_gates": ["h", "cx", "rz"],
+                "coupling_map": [[0, 1], [1, 2]],
+            }
+        }
+        sampler = SamplerV2(mode=FakeBrisbane(), options=opts_dict)
+
+        self.assertEqual(sampler.options.simulator.seed_simulator, 123)
+        self.assertEqual(sampler.options.simulator.basis_gates, ["h", "cx", "rz"])
+        self.assertEqual(sampler.options.simulator.coupling_map, CouplingMap([[0, 1], [1, 2]]))
 
     @data("bad_input", [1, 2, 3], [[0, 1], [-1, 0]])
     def test_coupling_map_invalid_type_raises(self, input):
@@ -53,7 +78,7 @@ class TestSimulatorOptions(IBMTestCase):
 
     def test_noise_model_invalid_type_no_aer_raises(self):
         """Passing a non-dict noise_model raises when Aer is not installed."""
-        with patch("qiskit_ibm_runtime.options_models.simulator.optionals.HAS_AER", False):
+        with patch("qiskit_ibm_runtime.options_models.simulator_options.optionals.HAS_AER", False):
             with self.assertRaises(ValidationError):
                 SimulatorOptions(noise_model=object())
 
