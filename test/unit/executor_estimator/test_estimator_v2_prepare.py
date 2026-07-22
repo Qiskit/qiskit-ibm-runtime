cfacc43ad67132958b5c0cf10d65880d042a5441
SamFerracin <sam.ferracin@ibm.com>
2026-07-22 16:19:56 -0400

    added factory

--- test/unit/executor_estimator/test_estimator_v2_prepare.py
+++ test/unit/executor_estimator/test_estimator_v2_prepare.py
@@ -18,7 +18,7 @@ import numpy as np
 from ddt import data, ddt, unpack
 from qiskit import QuantumCircuit
 from qiskit.circuit import ClassicalRegister, Parameter
-from qiskit.primitives.containers.estimator_pub import EstimatorPub, ObservablesArray
+from qiskit.primitives.containers import EstimatorPub, ObservablesArray
 from qiskit.quantum_info import SparsePauliOp
 
 from qiskit_ibm_runtime.exceptions import IBMInputValueError
@@ -31,11 +31,45 @@ from qiskit_ibm_runtime.quantum_program.quantum_program import SamplexItem
 
 from ...ibm_test_case import IBMTestCase
 from ...utils import combine
+from .utils import PARAM_BASIS_CASES_3Q
 
 
 @ddt
 class TestPrepare(IBMTestCase):
     """Tests for the prepare function."""
+
+    @data
+    def test_param_basis_expansion_3q(self):
+        """Test parameter-basis expansion with three-qubit observables."""
+        observables = PARAM_BASIS_CASES_3Q.observables
+        num_qubits = observables.num_qubits
+
+        circuit = QuantumCircuit(num_qubits)
+        circuit.rz(Parameter("alpha"), 0)
+
+        for case in PARAM_BASIS_CASES_3Q.cases:
+            parameter_shape = case.parameter_shape
+            observables_shape = case.observables_shape
+            expected_pairs = case.expected_pairs
+
+            with self.subTest(value=(parameter_shape, observables_shape, expected_pairs)):
+                pub_like = (
+                    circuit,
+                    observables.reshape(observables_shape),
+                    np.random.random(parameter_shape + (circuit.num_parameters,)),
+                )
+                pub = EstimatorPub.coerce(pub_like)
+
+                twirling_options = TwirlingOptions()
+                twirling_options.enable_gates = True
+                twirling_options.enable_measure = True
+                measure_noise_learning = MeasureNoiseLearningOptions()
+                program = prepare([pub], twirling_options, 10, measure_noise_learning)
+
+                post_processor_data = program.passthrough_data["post_processor"]
+                param_basis_pairs = post_processor_data["param_basis_pairs"][0]
+
+                self.assertListEqual(param_basis_pairs, expected_pairs, msg=param_basis_pairs)
 
     @data(
         [(2, 2), (2, 2), (1, 4)],
---
+++ test/unit/executor_estimator/utils.py
@@ -0,0 +1,89 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Utils for EstimatorV2 unit tests."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from qiskit.primitives.containers import ObservablesArray
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
+@dataclass(frozen=True)
+class BasisCase:
+    """A single parameter-basis expansion test case."""
+
+    parameter_shape: tuple[int, ...]
+    """The shape of the array of parameter values."""
+
+    observables_shape: tuple[int, ...]
+    """The shape of the observables array."""
+
+    expected_pairs: Sequence[tuple[tuple[int, ...], str]]
+    """Expected mapping from each parameter index to the observable(s) assigned after
+    broadcasting  the parameter and basis dimensions."""
+
+
+@dataclass(frozen=True)
+class ParamBasisTestCases:
+    """Test cases for parameter-basis expansion."""
+
+    observables: ObservablesArray
+    """The observables array used in the test case."""
+
+    cases: Sequence[BasisCase]
+    """A collection of test cases."""
+
+
+PARAM_BASIS_CASES_3Q = ParamBasisTestCases(
+    observables=ObservablesArray(["ZZZ", "XXX", "YYY", "IYI"]),
+    cases=[
+        BasisCase(
+            (2, 2),
+            (2, 2),
+            [((0, 0), "ZZZ"), ((0, 1), "XXX"), ((1, 0), "YYY"), ((1, 1), "IYI")],
+        ),
+        BasisCase(
+            (2, 2),
+            (2, 2, 1),
+            [
+                ((0, 0), "ZZZ"),
+                ((0, 0), "YYY"),
+                ((0, 1), "ZZZ"),
+                ((0, 1), "YYY"),
+                ((1, 0), "XXX"),
+                ((1, 0), "IYI"),
+                ((1, 1), "XXX"),
+                ((1, 1), "IYI"),
+            ],
+        ),
+        BasisCase(
+            (2, 2, 1),
+            (2, 2),
+            [
+                ((0, 0, 0), "ZZZ"),
+                ((0, 0, 0), "XXX"),
+                ((0, 1, 0), "YYY"),
+                ((1, 0, 0), "ZZZ"),
+                ((1, 0, 0), "XXX"),
+                ((1, 1, 0), "YYY"),
+            ],
+        ),
+        BasisCase((), (2, 2), [((), "ZZZ"), ((), "XXX"), ((), "YYY")]),
+    ],
+)
+"""Test cases for parameter-basis expansion with three-qubit observables."""
