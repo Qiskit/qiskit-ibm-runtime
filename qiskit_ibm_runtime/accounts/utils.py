3d1c2707d56c26ab1838612556872571047360b4
ElePT <epenatap@gmail.com>
2026-07-29 13:41:08 +0200

    Fix ruff E501 line-too-long linting errors

--- qiskit_ibm_runtime/accounts/utils.py
+++ qiskit_ibm_runtime/accounts/utils.py
@@ -30,14 +30,22 @@ def get_iam_api_url(cloud_url: str) -> str:
 def get_global_search_api_url(cloud_url: str, private_endpoint: bool = False) -> str:
     """Compute the GlobalSearchV2 API URL."""
     parsed_url = urlparse(cloud_url)
-    host = f"tags.private.global-search-tagging.{parsed_url.hostname}" if private_endpoint else f"api.global-search-tagging.{parsed_url.hostname}"
+    host = (
+        f"tags.private.global-search-tagging.{parsed_url.hostname}"
+        if private_endpoint
+        else f"api.global-search-tagging.{parsed_url.hostname}"
+    )
     return f"{parsed_url.scheme}://{host}"
 
 
 def get_global_catalog_api_url(cloud_url: str, private_endpoint: bool = False) -> str:
     """Compute the GlobalCatalogV1 API URL."""
     parsed_url = urlparse(cloud_url)
-    host = f"private.globalcatalog.{parsed_url.hostname}" if private_endpoint else f"globalcatalog.{parsed_url.hostname}"
+    host = (
+        f"private.globalcatalog.{parsed_url.hostname}"
+        if private_endpoint
+        else f"globalcatalog.{parsed_url.hostname}"
+    )
     return f"{parsed_url.scheme}://{host}/api/v1"
 
 
