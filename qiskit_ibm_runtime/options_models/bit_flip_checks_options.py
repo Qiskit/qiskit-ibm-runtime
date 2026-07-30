993e064c69dab235cbea0b63583ec56b890bf187
SamFerracin <sam.ferracin@ibm.com>
2026-07-30 14:13:00 -0400

    typo

--- qiskit_ibm_runtime/options_models/bit_flip_checks_options.py
+++ qiskit_ibm_runtime/options_models/bit_flip_checks_options.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Post selection options."""
+"""Options to apply bit-flip checks."""
 
 from typing import Literal
 
