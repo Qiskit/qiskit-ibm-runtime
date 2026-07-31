d9afff50119d42ffdb2cb9cc7116ee73021cb3cd
ElePT <epenatap@gmail.com>
2026-07-31 11:14:03 +0200

    Revert private_endpoint params from utils URL helper functions

--- qiskit_ibm_runtime/accounts/utils.py
+++ qiskit_ibm_runtime/accounts/utils.py
@@ -27,26 +27,16 @@ def get_iam_api_url(cloud_url: str) -> str:
     return f"{parsed_url.scheme}://iam.{parsed_url.hostname}"
 
 
-def get_global_search_api_url(cloud_url: str, private_endpoint: bool = False) -> str:
+def get_global_search_api_url(cloud_url: str) -> str:
     """Compute the GlobalSearchV2 API URL."""
     parsed_url = urlparse(cloud_url)
-    host = (
-        f"tags.private.global-search-tagging.{parsed_url.hostname}"
-        if private_endpoint
-        else f"api.global-search-tagging.{parsed_url.hostname}"
-    )
-    return f"{parsed_url.scheme}://{host}"
+    return f"{parsed_url.scheme}://api.global-search-tagging.{parsed_url.hostname}"
 
 
-def get_global_catalog_api_url(cloud_url: str, private_endpoint: bool = False) -> str:
+def get_global_catalog_api_url(cloud_url: str) -> str:
     """Compute the GlobalCatalogV1 API URL."""
     parsed_url = urlparse(cloud_url)
-    host = (
-        f"private.globalcatalog.{parsed_url.hostname}"
-        if private_endpoint
-        else f"globalcatalog.{parsed_url.hostname}"
-    )
-    return f"{parsed_url.scheme}://{host}/api/v1"
+    return f"{parsed_url.scheme}://globalcatalog.{parsed_url.hostname}/api/v1"
 
 
 def get_resource_controller_api_url(cloud_url: str) -> str:
