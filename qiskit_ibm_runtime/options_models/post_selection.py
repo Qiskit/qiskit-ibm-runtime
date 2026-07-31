a3b2265089579a8e855d43f4c83caadc630343a8
SamFerracin <sam.ferracin@ibm.com>
2026-07-31 15:14:27 -0400

    switching to annotated

--- qiskit_ibm_runtime/options_models/post_selection.py
+++ qiskit_ibm_runtime/options_models/post_selection.py
@@ -12,18 +12,30 @@
 
 """Post selection options."""
 
-from typing import Literal
+from typing import Annotated, Literal
 
-from pydantic import field_validator
+from pydantic import AfterValidator
 
 from ..utils.deprecation import issue_deprecation_msg
 from .base import BaseOptionsModel
 
 
+def _warn_post_selection(value: bool) -> bool:
+    """Warn that the post selection options are deprecated."""
+    if value:
+        issue_deprecation_msg(
+            msg="The 'post_selection' field of NoiseLearnerV3 is deprecated",
+            version="0.49.0",
+            remedy="Use 'bit_flip_check' field instead",
+            stacklevel=2,
+        )
+    return value
+
+
 class PostSelectionOptions(BaseOptionsModel):
     """Options for post selecting results."""
 
-    enable: bool = False
+    enable: Annotated[bool, AfterValidator(_warn_post_selection)] = False
     """Whether to enable Post Selection when performing learning experiments.
 
     If ``True``, Post Selection is applied to all the learning circuits. In particular, the
@@ -55,16 +67,3 @@ class PostSelectionOptions(BaseOptionsModel):
     See the dosctrings of :class:`.PostSelector` and :meth:`.PostSelector.compute_mask` for more
     details.
     """
-
-    @field_validator("enable")
-    @classmethod
-    def _warn_post_selection(cls, value: bool) -> bool:
-        """Warn that the post selection options are deprecated."""
-        if value:
-            issue_deprecation_msg(
-                msg="The 'post_selection' field of NoiseLearnerV3 is deprecated",
-                version="0.49.0",
-                remedy="Use 'bit_flip_check' field instead",
-                stacklevel=2,
-            )
-        return value
