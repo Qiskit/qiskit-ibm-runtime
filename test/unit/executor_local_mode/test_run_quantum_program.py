d04f0ec572b3d395596f9c01411e47f4abbb7e08
Yael Ben-Haim <yaelbh@il.ibm.com>
2026-08-02 11:45:07 +0300

    Change executor simulator metadata

--- test/unit/executor_local_mode/test_run_quantum_program.py
+++ test/unit/executor_local_mode/test_run_quantum_program.py
@@ -33,6 +33,7 @@ from samplomatic.transpiler import generate_boxing_pass_manager
 from qiskit_ibm_runtime.executor_local_mode.run_quantum_program import run_quantum_program
 from qiskit_ibm_runtime.fake_provider.backends.fez import FakeFez
 from qiskit_ibm_runtime.quantum_program import QuantumProgram
+from qiskit_ibm_runtime.results import QuantumProgramItemResult
 
 from ...ibm_test_case import IBMTestCase
 
@@ -268,6 +269,8 @@ class TestRunQuantumProgram(IBMTestCase):
 
         self.assertEqual(len(result), 1)
         item_data = result[0]
+        self.assertIsInstance(item_data, QuantumProgramItemResult)
+        self.assertIsNotNone(item_data.metadata)
 
         # Result shape: (4, shots, 2)
         self.assertEqual(item_data["c"].shape, (4, shots, 2))
