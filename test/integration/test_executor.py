5ec559ba7e10276fa5105c9e6a36bfa3d2dcef3c
Rainer Schoenberger <36071440+rainerschoe@users.noreply.github.com>
2026-08-03 10:50:36 +0200

    Update test/integration/test_executor.py
    Co-authored-by: Samuele Ferracin <sam.ferracin@ibm.com>

--- test/integration/test_executor.py
+++ test/integration/test_executor.py
@@ -19,7 +19,7 @@ from samplomatic.transpiler import generate_boxing_pass_manager
 
 from qiskit_ibm_runtime import Executor, QuantumProgram
 from qiskit_ibm_runtime.results import QuantumProgramItemResult, QuantumProgramResult
-from test.utils import make_mirror_circuit_with_phases
+from .utils import make_mirror_circuit_with_phases
 
 from ..ibm_test_case import IBMIntegrationTestCase
 
