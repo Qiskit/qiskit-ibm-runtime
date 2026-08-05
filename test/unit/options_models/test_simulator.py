d2366017d55f09447c21d537d1c785a6efa0420a
Anthony <125415978+apchytr@users.noreply.github.com>
2026-08-05 14:53:22 -0400

    Apply suggestion from @apchytr

--- test/unit/options_models/test_simulator.py
+++ test/unit/options_models/test_simulator.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Tests for SimulatorOptions and SimulatorOptions."""
+"""Tests for SimulatorOptions and ExperimentalSimulatorOptions."""
 
 from unittest.mock import patch
 
