f223e677362ca77599e3099ea34e1f07078c721f
SamFerracin <sam.ferracin@ibm.com>
2026-08-06 15:36:15 -0400

    modified test

--- test/unit/executor_sampler/simulations/test_sampler.py
+++ test/unit/executor_sampler/simulations/test_sampler.py
@@ -22,12 +22,7 @@ from qiskit.primitives.containers.sampler_pub import SamplerPub
 from qiskit.quantum_info import Statevector, hellinger_fidelity
 from qiskit_aer import AerSimulator
 
-from qiskit_ibm_runtime.decoders.executor_sampler.post_processor_v0_1 import (
-    sampler_v2_post_processor_v0_1,
-)
 from qiskit_ibm_runtime.executor_sampler import SamplerV2
-from qiskit_ibm_runtime.fake_provider import FakeManilaV2
-from qiskit_ibm_runtime.sim_executor import SimExecutor
 
 from ....ibm_test_case import IBMTestCase
 from ....utils import make_mirror_circuit_with_phases
@@ -47,7 +42,7 @@ class TestSampler(IBMTestCase):
         """Test level setup."""
         super().setUp()
 
-        self.backend = FakeManilaV2()
+        self.backend = AerSimulator()
 
         # The tolerance used when verifying simulated counts with exact probabilities via
         # Hellinger distance.
@@ -90,10 +85,8 @@ class TestSampler(IBMTestCase):
 
         pubs = [SamplerPub.coerce([circuit, parameters])]
 
-        # TODO: This is a temporary patch that can be removed when sampler supports
-        # local mode via executor's own local mode.
-        sampler = SamplerV2(self.backend)
-        sampler._executor = SimExecutor(AerSimulator())
+        sampler = SamplerV2(self.backend, options={"experimental": {"local_mode": True}})
+        # sampler.options.experimental = {"local_mode": True}
 
         sampler.options.twirling.enable_gates = enable_gates
         sampler.options.twirling.enable_measure = enable_measure
@@ -104,8 +97,8 @@ class TestSampler(IBMTestCase):
 
         # TODO: This is a temporary patch that can be removed when sampler supports
         # local mode via executor's own local mode.
-        executor_result = job.result()
-        result = sampler_v2_post_processor_v0_1(executor_result)
+        result = job.result()
+        # result = sampler_v2_post_processor_v0_1(executor_result)
 
         array = result[0].data.meas
         self.assertEqual(
