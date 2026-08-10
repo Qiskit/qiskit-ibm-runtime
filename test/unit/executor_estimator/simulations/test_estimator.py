4e5ad19a7603260d23ae82348cafefdf208f0db4
Rainer Schoenberger <rschoe@de.ibm.com>
2026-08-10 22:44:01 +0200

    increase absolute allowed error

--- test/unit/executor_estimator/simulations/test_estimator.py
+++ test/unit/executor_estimator/simulations/test_estimator.py
@@ -108,4 +108,4 @@ class TestEstimator(IBMTestCase):
         np.testing.assert_array_less(errors[1], errors[0], err_msg=debug_message)
 
         # Resilience level 2 should give very accurate expectation value:
-        np.testing.assert_array_less(errors[2], 0.05, err_msg=debug_message)
+        np.testing.assert_array_less(errors[2], 0.1, err_msg=debug_message)
