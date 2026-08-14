fe6ab488cc4a486a5e76a9896e1db2e3127db9fb
Rainer Schoenberger <36071440+rainerschoe@users.noreply.github.com>
2026-08-14 18:12:13 +0000

    Simtest: Parametrize tests to run PEC + PEA (#3235)
    <!--
    ⚠️ The pull request title should be short detailed and understandable
    for all.
    ⚠️ Also, please add a release note file using towncrier if the change
    needs to
      be documented in the release notes.
    ⚠️ If your pull request fixes an open issue, please link to the issue.
    
    ✅ I have added the tests to cover my changes.
    ✅ I have updated the documentation accordingly.
    ✅ I have read the CONTRIBUTING document.
    -->
    
    ### Summary
    refactoring the test a bit to make it parametrized. Now it can also
    cover PEA, as well as other executor configs.
    
    ### Details and comments
    
    
    ### AI/LLM disclosure
    
    - [x] I didn't use LLM tooling, or only used it privately.
    - [ ] I used the following tool to help write this PR description:
    - [ ] I used the following tool to generate or modify code:
    
    ---------
    
    Co-authored-by: SamFerracin <sam.ferracin@ibm.com>

--- test/unit/executor_estimator/simulations/utils.py
+++ test/unit/executor_estimator/simulations/utils.py
@@ -16,7 +16,9 @@ from __future__ import annotations
 
 import numpy as np
 from qiskit.circuit import Parameter
-from qiskit.quantum_info import Operator, SparsePauliOp
+from qiskit.quantum_info import Operator, PauliLindbladMap, SparsePauliOp
+from samplomatic import InjectNoise
+from samplomatic.utils import get_annotation
 
 from qiskit_ibm_runtime.executor_estimator import EstimatorV2
 from qiskit_ibm_runtime.options_models.estimator import EstimatorOptions
@@ -24,11 +26,60 @@ from qiskit_ibm_runtime.options_models.estimator import EstimatorOptions
 from ....utils import make_mirror_circuit_with_phases
 
 
-def create_estimator_test_data_big(backend, preset_pass_manager):
+def create_estimator_test_data(backend, preset_pass_manager):
+    """Create a pub and ideal expectation values for it.
+
+    The circuit will use 3 qubits and only a subset of observable combinations.
+    """
+    circuit = make_mirror_circuit_with_phases(
+        backend, num_qubits=3, add_measurement=False, add_rx=False
+    )
+
+    circuit.rx(Parameter("rx_0"), 0)
+    circuit.rx(Parameter("rx_1"), 1)
+    circuit.ry(Parameter("ry_2"), 2)
+    isa_circuit = preset_pass_manager.run(circuit)
+
+    theta = np.pi / 5
+    phi = np.pi / 3
+    parameters = [theta, -phi, 3 * np.pi / 4]
+
+    y_q1 = np.sin(phi)
+    z_q0 = np.cos(theta)
+    r_q1 = (1 + np.sin(phi)) / 2
+    l_q0 = (1 + np.sin(theta)) / 2
+    x_q2 = np.sqrt(2) / 2
+
+    observable_ideal_ev_pairs: list[tuple[str, float]] = [
+        ("IYZ", y_q1 * z_q0),  # ≈ 0.701
+        ("Irl", r_q1 * l_q0),  # ≈ 0.741
+        ("XII", x_q2),  # ≈ 0.707
+    ]
+
+    # FIXME: Composing observables from plain `Operator` instead of directly passing strings,
+    # due to a bug in TREX post-processing affecting resilience levels > 0:
+    # https://github.com/Qiskit/qiskit-ibm-runtime/issues/3225
+    # Once this is fixed, we can do:
+    # observables = [obs_string for obs_string, _ in observable_ideal_ev_pairs]
+    observables = [
+        SparsePauliOp.from_operator(Operator.from_label(obs_string)).apply_layout(
+            isa_circuit.layout
+        )
+        for obs_string, _ in observable_ideal_ev_pairs
+    ]
+
+    pub = (isa_circuit, observables, parameters)
+
+    return pub, [ev for _, ev in observable_ideal_ev_pairs]
+
+
+def create_estimator_test_data_extended(backend, preset_pass_manager):
     """Create a pub and ideal expectation values for it.
 
     The circuit will use 4 qubits and try to use an extensive list of observables to provide
     coverage.
+    Due a bigger number of qubits and observables, expect longer runtimes when using it,
+    especially in noisy simulations.
     """
     circuit = make_mirror_circuit_with_phases(
         backend, num_qubits=4, add_measurement=False, add_rx=False
@@ -82,51 +133,24 @@ def create_estimator_test_data_big(backend, preset_pass_manager):
     return pub, [ev for _, ev in observable_ideal_ev_pairs]
 
 
-def create_estimator_test_data_small(backend, preset_pass_manager):
-    """Create a pub and ideal expectation values for it.
-
-    The circuit will use 3 qubits and only a subset of observable combinations.
-    """
-    circuit = make_mirror_circuit_with_phases(
-        backend, num_qubits=3, add_measurement=False, add_rx=False
-    )
-
-    circuit.rx(Parameter("rx_0"), 0)
-    circuit.rx(Parameter("rx_1"), 1)
-    circuit.ry(Parameter("ry_2"), 2)
-    isa_circuit = preset_pass_manager.run(circuit)
-
-    theta = np.pi / 5
-    phi = np.pi / 3
-    parameters = [theta, -phi, 3 * np.pi / 4]
-
-    y_q1 = np.sin(phi)
-    z_q0 = np.cos(theta)
-    r_q1 = (1 + np.sin(phi)) / 2
-    l_q0 = (1 + np.sin(theta)) / 2
-    x_q2 = np.sqrt(2) / 2
-
-    observable_ideal_ev_pairs: list[tuple[str, float]] = [
-        ("IYZ", y_q1 * z_q0),  # ≈ 0.701
-        ("Irl", r_q1 * l_q0),  # ≈ 0.741
-        ("XII", x_q2),  # ≈ 0.707
+def create_noise_model_without_noise(estimator, pub):
+    """Creates a noise-model, mapping each layer to the identity (no noise)."""
+    layers = [
+        layer
+        for layer in estimator.find_unique_layers([pub])
+        if get_annotation(layer.operation, InjectNoise)
     ]
 
-    # FIXME: Composing observables from plain `Operator` instead of directly passing strings,
-    # due to a bug in TREX post-processing affecting resilience levels > 0:
-    # https://github.com/Qiskit/qiskit-ibm-runtime/issues/3225
-    # Once this is fixed, we can do:
-    # observables = [obs_string for obs_string, _ in observable_ideal_ev_pairs]
-    observables = [
-        SparsePauliOp.from_operator(Operator.from_label(obs_string)).apply_layout(
-            isa_circuit.layout
+    # In a noise-less simulation we do not expect noise. So we can construct the noise_model
+    # with empty noise for all layers:
+    noise_model = {
+        get_annotation(layer.operation, InjectNoise).ref: PauliLindbladMap.identity(
+            layer.operation.num_qubits
         )
-        for obs_string, _ in observable_ideal_ev_pairs
-    ]
+        for layer in layers
+    }
 
-    pub = (isa_circuit, observables, parameters)
-
-    return pub, [ev for _, ev in observable_ideal_ev_pairs]
+    return noise_model
 
 
 def create_local_mode_estimator(backend):
