96abbab12227d1560300c9b52238a8c3488df921
Tsafrir Armon <tsafrir.armon@ibm.com>
2026-08-20 14:37:31 +0300

    fix imports

--- qiskit_ibm_runtime/executor_estimator/utils.py
+++ qiskit_ibm_runtime/executor_estimator/utils.py
@@ -26,7 +26,8 @@ if TYPE_CHECKING:
     import numpy.typing as npt
     from qiskit import QuantumCircuit
     from qiskit.circuit import BoxOp, CircuitInstruction
-    from qiskit.primitives.containers import EstimatorPub, SamplerPub
+    from qiskit.primitives.containers.estimator_pub import EstimatorPub
+    from qiskit.primitives.containers.sampler_pub import SamplerPub
     from samplomatic.samplex import Samplex
 
     from ..options_models.measure_noise_learning import MeasureNoiseLearningOptions
--- test/unit/executor_estimator/simulations/utils.py
+++ test/unit/executor_estimator/simulations/utils.py
@@ -19,7 +19,7 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 from qiskit.circuit import Parameter
-from qiskit.primitives.containers import EstimatorPub
+from qiskit.primitives.containers.estimator_pub import EstimatorPub
 from qiskit.quantum_info import (
     Operator,
     PauliLindbladMap,
