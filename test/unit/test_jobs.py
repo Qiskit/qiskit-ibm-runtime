7eeafe555a7d51f2a9a81e02e3eead23a1a89d56
Diego M. Rodriguez <diego@moreda.io>
2026-09-09 13:37:03 +0200

    Revise test_jobs.py

--- test/unit/test_jobs.py
+++ test/unit/test_jobs.py
@@ -12,8 +12,11 @@
 
 """Tests for job related runtime functions."""
 
+from __future__ import annotations
+
 import json
 import warnings
+from typing import TYPE_CHECKING
 from unittest.mock import patch
 
 from ddt import data, ddt
@@ -30,16 +33,12 @@ from qiskit_ibm_runtime.exceptions import (
 )
 from qiskit_ibm_runtime.qiskit_runtime_service import QiskitRuntimeService
 
-from ..decorators import mock_responses, run_cloud_fake
+from ..decorators import mock_responses
 from ..ibm_test_case import IBMTestCase
-from ..program import run_program
 from ..registries import Job
-from ..utils import mock_wait_for_final_state
-from .mock.fake_runtime_client import (
-    CancelableRuntimeJob,
-    FailedRanTooLongRuntimeJob,
-    FailedRuntimeJob,
-)
+
+if TYPE_CHECKING:
+    from ..registries import BaseRegistry
 
 
 class ToIntDecoder(ResultDecoder):
@@ -60,122 +59,217 @@ class MultiplierDecoder(ResultDecoder):
         return data * 3
 
 
+def run_program(
+    service: QiskitRuntimeService,
+    registry: BaseRegistry,
+    inputs: dict,
+    options: dict,
+    backend_name: str = "common_backend",
+) -> tuple[RuntimeJobV2, Job]:
+    """Run a program using the `service`, and add a corresponding job to the registry.
+
+    This function takes advantage of the `QiskitRuntimeService._run()` method as the entry point
+    for executing a job, converting this function's arguments into the form it expects. It also
+    adds a corresponding registry `Job` to the `registry`.
+
+    Args:
+        service: the `QiskitRuntimeService` to use for running the program.
+        registry: the mocked registry for the decorated test.
+        inputs: input parameters.
+        options: runtime options.
+        backend_name: name of the backend to run against.
+
+    Returns:
+        The real job which results of the execution of the program, and the registry job that is
+        added to the registry.
+    """
+    options.update({"backend": backend_name, "instance": registry.instances["a"].crn})
+    job = service._run("sampler", inputs, options)
+    registry_job = Job(
+        job.job_id(),
+        backend_name,
+        program="sampler",
+        status="running",
+        statuses=["running", "completed"],
+    )
+    registry.add_job(registry_job, "a")
+
+    return job, registry_job
+
+
 @ddt
 class TestRuntimeJob(IBMTestCase):
     """Class for testing runtime jobs."""
 
-    @run_cloud_fake
-    def test_run_program(self, service):
+    @mock_responses
+    def test_run_program(self, registry):
         """Test running program."""
-        params = {"param1": "foo"}
-        job = run_program(service=service, inputs=params)
+        service = QiskitRuntimeService(token="my_token")
+        job, _ = run_program(service, registry, {"param1": "foo"}, {})
+
         self.assertTrue(job.job_id())
         self.assertIsInstance(job, RuntimeJobV2)
-        with mock_wait_for_final_state(service, job):
-            job.wait_for_final_state()
-            self.assertEqual(job.status(), "DONE")
-            self.assertTrue(job.result())
+        job.wait_for_final_state(poll_interval=0.1)
+        self.assertEqual(job.status(), "DONE")
+        self.assertTrue(job.result())
 
-    @run_cloud_fake
-    def test_run_program_phantom_backend(self, service):
+    @mock_responses
+    def test_run_program_phantom_backend(self, registry):
         """Test running on a phantom backend."""
+        service = QiskitRuntimeService(token="my_token")
         with self.assertRaises(QiskitBackendNotFoundError):
-            _ = run_program(service=service, backend_name="phantom_backend")
+            run_program(service, registry, {}, {}, backend_name="phantom_backend")
 
-    @run_cloud_fake
-    def test_run_program_with_custom_runtime_image(self, service):
+    @mock_responses
+    def test_run_program_with_custom_runtime_image(self, registry):
         """Test running program with a custom image."""
-        params = {"param1": "foo"}
         image = "name:tag"
-        job = run_program(service=service, inputs=params, image=image)
+        service = QiskitRuntimeService(token="my_token")
+        job, _ = run_program(service, registry, {"param1": "foo"}, {"image": image})
+
         self.assertTrue(job.job_id())
         self.assertIsInstance(job, RuntimeJobV2)
-        with mock_wait_for_final_state(service, job):
-            job.wait_for_final_state()
-            self.assertTrue(job.result())
+        job.wait_for_final_state(poll_interval=0.1)
+        self.assertTrue(job.result())
         self.assertEqual(job.status(), "DONE")
         self.assertEqual(job.image, image)
 
-    @run_cloud_fake
-    def test_run_program_with_custom_log_level(self, service):
-        """Test running program with a custom image."""
-        job = run_program(service=service, log_level="DEBUG")
-        job_raw = service._get_api_client()._get_job(job.job_id())
-        self.assertEqual(job_raw.log_level, "DEBUG")
+    @mock_responses
+    def test_run_program_with_custom_log_level(self, registry):
+        """Test running program with a custom log level."""
+        service = QiskitRuntimeService(token="my_token")
+        job, _ = run_program(service, registry, {"param1": "foo"}, {"log_level": "DEBUG"})
 
-    @run_cloud_fake
-    def test_run_program_failed(self, service):
-        """Test a failed program execution."""
-        job = run_program(service=service, job_classes=FailedRuntimeJob)
-        with mock_wait_for_final_state(service, job):
-            job.wait_for_final_state()
-            job_result_raw = service._get_api_client().job_results(job.job_id())
-            self.assertEqual("ERROR", job.status())
-            self.assertEqual(
-                API_TO_JOB_ERROR_MESSAGE["FAILED"].format(job.job_id(), job_result_raw),
-                job.error_message(),
-            )
-            with self.assertRaises(RuntimeJobFailureError):
-                job.result()
+        # `log_debug` is not returned by the API, nor present in `RuntimeJobV2`. The assert on
+        # this tests just check that the job is valid (and the "log_level" argument was accepted).
+        self.assertTrue(job.job_id())
+        self.assertIsInstance(job, RuntimeJobV2)
+        job.wait_for_final_state(poll_interval=0.1)
+        self.assertTrue(job.result())
+        self.assertEqual(job.status(), "DONE")
 
-    @run_cloud_fake
-    def test_run_program_failed_ran_too_long(self, service):
+    @mock_responses
+    def test_run_program_failed_with_no_reason(self, registry):
+        """Test a failed program execution, when no reason is present in the job details."""
+        service = QiskitRuntimeService(token="my_token")
+        job, registry_job = run_program(service, registry, {}, {})
+
+        # Update the registry job so it fails.
+        registry_job.statuses = ["running", "failed"]
+        registry_job.statuses_reason = {"failed": (123, "")}
+        registry_job.raw_results = json.dumps("Content from results")
+
+        job.wait_for_final_state(poll_interval=0.1)
+        job_result_raw = service._get_api_client().job_results(job.job_id())
+        self.assertEqual("ERROR", job.status())
+        self.assertEqual(
+            API_TO_JOB_ERROR_MESSAGE["FAILED"].format(job.job_id(), job_result_raw),
+            job.error_message(),
+        )
+
+        # When no reason is present in the job details, the error message is based on the raw
+        # response from the results.
+        self.assertIn("Content from results", job.error_message())
+        with self.assertRaisesRegex(RuntimeJobFailureError, "Content from results"):
+            job.result()
+
+    @mock_responses
+    def test_run_program_failed_with_reason(self, registry):
+        """Test a failed program execution, when reason is present in the job details."""
+        service = QiskitRuntimeService(token="my_token")
+        job, registry_job = run_program(service, registry, {}, {})
+
+        # Update the registry job so it fails.
+        registry_job.statuses = ["running", "failed"]
+        registry_job.statuses_reason = {"failed": (123, "Some reason")}
+
+        job.wait_for_final_state(poll_interval=0.1)
+        self.assertEqual("ERROR", job.status())
+
+        # When a reason is present in the job details, the error message is based on that reason.
+        self.assertIn("Some reason", job.error_message())
+        with self.assertRaisesRegex(RuntimeJobFailureError, "Some reason"):
+            job.result()
+
+    @mock_responses
+    def test_run_program_failed_ran_too_long(self, registry):
         """Test a program that failed since it ran longer than maximum execution time."""
-        job = run_program(service=service, job_classes=FailedRanTooLongRuntimeJob)
-        with mock_wait_for_final_state(service, job):
-            job.wait_for_final_state()
-            job_result_raw = service._get_api_client().job_results(job.job_id())
-            self.assertEqual("ERROR", job.status())
-            self.assertEqual(
-                API_TO_JOB_ERROR_MESSAGE["CANCELLED - RAN TOO LONG"].format(
-                    job.job_id(), job_result_raw
-                ),
-                job.error_message(),
-            )
-            with self.assertRaises(RuntimeJobMaxTimeoutError):
-                job.result()
+        service = QiskitRuntimeService(token="my_token")
+        job, registry_job = run_program(service, registry, {}, {})
 
-    @run_cloud_fake
-    def test_cancel_job(self, service):
+        # Update the registry job so it fails.
+        registry_job.statuses = ["running", "cancelled"]
+        registry_job.statuses_reason = {"cancelled": (1305, "RAN TOO LONG")}
+        registry_job.raw_results = json.dumps("Content from results")
+
+        job.wait_for_final_state(poll_interval=0.1)
+        job_result_raw = service._get_api_client().job_results(job.job_id())
+        self.assertEqual("ERROR", job.status())
+        self.assertEqual(
+            API_TO_JOB_ERROR_MESSAGE["CANCELLED - RAN TOO LONG"].format(
+                job.job_id(), job_result_raw
+            ),
+            job.error_message(),
+        )
+
+        # In this specific case, the error message is based on the content from the results, and the
+        # exception is based on the reason.
+        self.assertIn("Content from results", job.error_message())
+        with self.assertRaisesRegex(RuntimeJobMaxTimeoutError, "RAN TOO LONG"):
+            job.result()
+
+    @mock_responses
+    def test_cancel_job(self, registry):
         """Test canceling a job."""
-        job = run_program(service, job_classes=CancelableRuntimeJob)
+        service = QiskitRuntimeService(token="my_token")
+        job, registry_job = run_program(service, registry, {}, {})
+
+        # Update the registry job so it gets cancelled.
+        registry_job.statuses = ["running", "cancelled"]
+
         job.cancel()
         self.assertEqual(job.status(), "CANCELLED")
-        rjob = service.job(job.job_id())
-        self.assertEqual(rjob.status(), "CANCELLED")
-        with self.assertRaises(RuntimeInvalidStateError) as exc:
-            rjob.result()
-        self.assertIn("Job was cancelled", str(exc.exception))
 
-    @run_cloud_fake
-    def test_final_result(self, service):
+        # This automatically advances the job to cancelled.
+        remote_job = service.job(job.job_id())
+        self.assertEqual(remote_job.status(), "CANCELLED")
+        with self.assertRaisesRegex(RuntimeInvalidStateError, "Job was cancelled"):
+            remote_job.result()
+
+    @mock_responses
+    def test_final_result(self, registry):
         """Test getting final result."""
-        job = run_program(service)
-        with mock_wait_for_final_state(service, job):
-            result = job.result()
-            self.assertTrue(result)
+        service = QiskitRuntimeService(token="my_token")
+        job, _ = run_program(service, registry, {}, {})
+        job.wait_for_final_state(poll_interval=0.1)
+        result = job.result()
+        self.assertTrue(result)
 
-    @run_cloud_fake
-    def test_job_status(self, service):
+    @mock_responses
+    def test_job_status(self, registry):
         """Test job status."""
-        job = run_program(service)
+        service = QiskitRuntimeService(token="my_token")
+        job, _ = run_program(service, registry, {}, {})
         self.assertTrue(job.status())
 
-    @run_cloud_fake
-    def test_wait_for_final_state(self, service):
+    @mock_responses
+    def test_wait_for_final_state(self, registry):
         """Test wait for final state."""
-        job = run_program(service)
-        with mock_wait_for_final_state(service, job):
-            job.wait_for_final_state()
+        service = QiskitRuntimeService(token="my_token")
+        job, _ = run_program(service, registry, {}, {})
+        job.wait_for_final_state(poll_interval=0.1)
         self.assertEqual("DONE", job.status())
 
-    @run_cloud_fake
-    def test_delete_job(self, service):
+    @mock_responses
+    def test_delete_job(self, registry):
         """Test deleting a job."""
-        params = {"param1": "foo"}
-        job = run_program(service=service, inputs=params)
+        service = QiskitRuntimeService(token="my_token")
+        job, registry_job = run_program(service, registry, {"param1": "foo"}, {})
         self.assertTrue(job.job_id())
+
         service.delete_job(job.job_id())
+
+        del registry.jobs["a"][registry_job.id]
         with self.assertRaises(RuntimeJobNotFound):
             service.job(job.job_id())
 
@@ -210,24 +304,27 @@ class TestRuntimeJob(IBMTestCase):
         with self.assertWarnsRegex(UserWarning, r"There is currently no more time available"):
             service._run(program_id="sampler", options={"backend": "common_backend"}, inputs={})
 
-    @run_cloud_fake
     @data((None, 0.5), ("some_session_id", 0.1))
-    def test_wait_for_final_state_poll_interval_defaults(self, id_and_default, service):
+    @mock_responses
+    def test_wait_for_final_state_poll_interval_defaults(self, id_and_default, registry):
         """wait_for_final_state should use default values if `None` provided."""
-        session_id, default = id_and_default
+        service = QiskitRuntimeService(token="my_token")
+        job, _ = run_program(service, registry, {}, {})
 
-        job = run_program(service)
+        session_id, default = id_and_default
         job._session_id = session_id
         with patch.object(RuntimeJobV2, "status", side_effect=["WAITING", "DONE"]):
             with patch("time.sleep", return_value=None) as patched_sleep:
                 job.wait_for_final_state()
                 patched_sleep.assert_called_with(default)
 
-    @run_cloud_fake
     @data(0.99, 0.1, 1.23)
-    def test_wait_for_final_state_poll_inverval_custom(self, value, service):
+    @mock_responses
+    def test_wait_for_final_state_poll_inverval_custom(self, value, registry):
         """Test wait_for_final_state when passing a custom poll interval."""
-        job = run_program(service)
+        service = QiskitRuntimeService(token="my_token")
+        job, _ = run_program(service, registry, {}, {})
+
         expected_interval = value
         with patch.object(RuntimeJobV2, "status", side_effect=["WAITING", "DONE"]):
             with patch("time.sleep", return_value=None) as patched_sleep:
@@ -268,13 +365,15 @@ class TestRuntimeJob(IBMTestCase):
         usage = job.usage(partial=True)
         self.assertEqual(usage, 123)
 
-    @run_cloud_fake
     @data(None, ToIntDecoder, [ToIntDecoder, MultiplierDecoder])
-    def test_result_decoders_is_always_sequence(self, decoders, service):
+    @mock_responses
+    def test_result_decoders_is_always_sequence(self, decoders, registry):
         """`job._result_decoders` should always be a list after instantiating."""
+        service = QiskitRuntimeService(token="my_token")
+
         job = RuntimeJobV2(
             backend=None,
-            api_client=service._fake_runtime_client,
+            api_client=service._active_api_client,
             job_id="123",
             program_id="blah",
             service=service,
