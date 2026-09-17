a5fa8895d1e7e81f5d6d56377d4bea03ec582293
Diego M. Rodriguez <diego@moreda.io>
2026-09-17 15:57:59 +0200

    Solve remaining naming conflicts

--- qiskit_ibm_runtime/sampler.py
+++ qiskit_ibm_runtime/sampler.py
@@ -31,6 +31,7 @@ if TYPE_CHECKING:
     from qiskit.primitives.containers.sampler_pub import SamplerPubLike
     from qiskit.providers import BackendV2
 
+    from . import options as legacy_options  # Needed as sphinx is unable to solve conflicts.
     from .batch import Batch
     from .runtime_job_v2 import RuntimeJobV2
     from .session import Session
@@ -79,9 +80,9 @@ class SamplerV2(BasePrimitiveV2[SamplerOptions], Sampler, BaseSamplerV2):
     def __init__(
         self,
         mode: BackendV2 | Session | Batch | None = None,
-        options: dict | SamplerOptions | None = None,
+        options: dict | legacy_options.SamplerOptions | None = None,
     ):
-        self.options: SamplerOptions
+        self.options: legacy_options.SamplerOptions
         BaseSamplerV2.__init__(self)
         Sampler.__init__(self)
 
