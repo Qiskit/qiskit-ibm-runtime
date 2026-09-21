d3a41595d680bf7750b0fdc4da1489919d6ad92b
Diego M. Rodriguez <diego@moreda.io>
2026-09-21 20:47:10 +0200

    Pass proxies and verify to resolve_crn

--- qiskit_ibm_runtime/accounts/account.py
+++ qiskit_ibm_runtime/accounts/account.py
@@ -317,6 +317,8 @@ class CloudAccount(Account):
             url=self.url,
             token=self.token,
             instance=self.instance,
+            proxies_kwargs=self._get_proxies_kwargs(),
+            verify=bool(self.verify),
         )
         if len(crn) == 0:
             raise CloudResourceNameResolutionError(
