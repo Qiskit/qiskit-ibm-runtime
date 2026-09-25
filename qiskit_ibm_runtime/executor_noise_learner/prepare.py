08b0f9375a62e49391062b1c9b07dadffa34d4f4
Anthony <anthonychytros@gmail.com>
2026-09-25 15:24:08 -0400

    pre-commit

--- qiskit_ibm_runtime/executor_noise_learner/prepare.py
+++ qiskit_ibm_runtime/executor_noise_learner/prepare.py
@@ -53,7 +53,7 @@ def prepare(
     """
     executor_options = noise_learner_options_to_executor_options(options)
     quantum_program = QuantumProgram(shots=1)
-    
+
     circuit = QuantumCircuit(1)
     circuit.measure_all()
     quantum_program.append_circuit_item(circuit)
